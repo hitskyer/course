f7a9b08fc6a6a112dcc15b3a18f22c8d006958c2
liuyunpeng2004 <1744229126@qq.com>
2018-01-29 22:05:09 +0800

    从徐兴军目录中同步三个文件的最新版本

--- c/liuyunpeng/chapter01/cout_cerr_clog.cpp
+++ c/liuyunpeng/chapter01/cout_cerr_clog.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * 1. 课程范围："c++ primer"第4班第1.2.1节
+ * 2. 课后练习：
+ *    a) 描述"using namespace std;"给后继程序带来的变化
+ *    b) g++ cout_cerr_clog.cpp -o out && ./out >& log.all && ./out > log.out 2> log.err
+ *    c) 观察log.all，描述值得注意的现象，理解缓冲区、强制清空的概念；
+ *    d) 观察log.out与log.err的区别，理解标准输出流、标准错误流；
+ */
 int main(void)
 {
 	// cout输出到标准输出流，>
@@ -25,18 +33,20 @@ int main(void)
 
 	cout << "--------" << endl;
 
-	cout << "cout 5\n";
-	clog << "clog 3\n";
+	// 每次输出，都会强制清空缓冲区
+	cout << "cout 5" << endl;
+	clog << "clog 3" << endl;
 	
-	cout << "cout 6\n";
-	clog << "clog 4\n";
+	cout << "cout 6" << endl;
+	clog << "clog 4" << endl;
 
 	cout << "--------" << endl;
 
-	cout << "cout 7\n";
-	cerr << "cerr 3\n";
+	// 每次输出，都会强制清空缓冲区
+	cout << "cout 7" << endl;
+	cerr << "cerr 3" << endl;
 	
-	cout << "cout 8\n";
-	cerr << "cerr 4\n";
+	cout << "cout 8" << endl;
+	cerr << "cerr 4" << endl;
 	return 0;
 }
