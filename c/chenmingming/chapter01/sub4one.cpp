bea1c46c553e880220ade783fb8998d009238d6d
kobe24o <mingmingchen1989@163.com>
2018-02-04 22:57:17 +0800

    修复程序BUG

--- c/chenmingming/chapter01/sub4one.cpp
+++ c/chenmingming/chapter01/sub4one.cpp
@@ -2,16 +2,22 @@
 int main()
 {
 	using namespace std;
-	int x=0 , y=0;
-	string s;
+	int x=0 , y=0;			//定义两个变量
+	string s;			//定义程序”是否继续“字符s
 	do
 	{
 	cout << "-------------------------------\n请输入两个数做减法！\n";
-	cin >> x >> y;
+		while(!(cin >> x && cin >> y))	//判断是否都为数字
+			{
+			cout << "输入了非数字，请输入两个数字！！！\n" << endl;
+			cin.clear();		//让错误标识改回为0,让我们可以继续输入
+			cin.sync();		//清空已输入的数据
+			cin.get();		//让程序等待输入
+			}
 	cout << "两数之差为：" << x-y << "\n";
 	cout << "输入y或者Y继续计算\n输入其他任意字符退出程序。\n" << endl;
 	cin >> s;
 	}
-	while(s == "y" || s == "Y");
+	while(s == "y" || s == "Y");		//输入y或Y继续执行程序
 	return 0;
 }
