07f716059809f7a69b34c1bdda0f2088c590be59
kobe24o <mingmingchen1989@163.com>
2018-02-05 22:28:36 +0800

    fix bug

--- c/chenmingming/chapter01/sub4one.cpp
+++ c/chenmingming/chapter01/sub4one.cpp
@@ -7,17 +7,16 @@ int main()
 	do
 	{
 	cout << "-------------------------------\n请输入两个数做减法！\n";
-		while(!(cin >> x && cin >> y))	//判断是否都为数字
+		while(!(cin >> x >>y))	//判断是否都为数字
 			{
 			cout << "输入了非数字，请输入两个数字！！！" << endl;
 			cin.clear();		//让错误标识改回为0,让我们可以继续输入
-			cin.sync();		//清空已输入的数据
-			cin.get();		//让程序等待输入
+			cin.ignore(1000,'\n');  //清空已输入的数据
 			}
 	cout << "两数之差为：" << x-y << "\n";
 	cout << "输入y或者Y继续计算\n输入其他任意字符退出程序。\n" << endl;
-	cin >> s;
+	//cin >> s;
 	}
-	while(s == "y" || s == "Y");		//输入y或Y继续执行程序
+	while(cin.get() == 'y' || cin.get() == 'Y');		//输入y或Y继续执行程序
 	return 0;
 }
