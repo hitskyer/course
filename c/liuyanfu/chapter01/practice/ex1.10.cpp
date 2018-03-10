1446ce3a98a534fc3e876cd2c7d70e069e0c7dc0
RF-LYF <1614746864@qq.com>
2018-03-10 19:30:49 +0800

    exercise

---
+++ c/liuyanfu/chapter01/practice/ex1.10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+ using namespace std;
+
+   int main ()
+      {
+         int i=10;
+         while (i>=0)
+          {
+            cout << i << endl; 
+            i--;
+          }
+         return 0;
+       }
