b3a2b8f9f335102a88602765d6046c9b5badce38
徐兴军 <xxjroom@163.com>
2018-03-24 18:43:01 +0800

    获取各种类型的存储大小

---
+++ c/xuxingjun/chapter02/sizeof.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "the size of char is      " << sizeof(char) << " bytes" << endl;
+	cout << "the size of bool is      " << sizeof(bool) << " bytes" << endl;
+	cout << "the size of short is     " << sizeof(short) << " bytes" << endl;
+	cout << "the size of int is       " << sizeof(int) << " bytes" << endl;
+	cout << "the size of long is      " << sizeof(long) << " bytes" << endl;
+	cout << "the size of long long is " << sizeof(long long) << " bytes" << endl;
+	cout << "the size of float is     " << sizeof(float) << " bytes" << endl;
+	cout << "the size of double is    " << sizeof(double) << " bytes" << endl;
+	cout << "the size of int* is      " << sizeof(int*) << " bytes" << endl;
+	cout << "the size of double* is   " << sizeof(double*) << " bytes" << endl;
+	
+	return 0;
+}
