e44d5a804609893d1f00ff5594a7729c489deb96
kobe24o <mingmingchen1989@163.com>
2018-03-25 08:50:56 +0800

    homeworks

---
+++ c/chenmingming/chapter01/avg_price.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+	Sales_item total;
+	if(std::cin >> total)
+		{
+		Sales_item trans;
+			while(std::cin >> trans)
+				{
+				if(total.isbn() == trans.isbn())
+					total += trans;
+				else
+					{
+					std::cout << total << std::endl;
+					total = trans;
+					}
+				}
+		std::cout << total << std::endl;
+		}
+	else
+		{
+		std::cerr << "no data ?!" << std::endl;
+		return -1;
+		}
+	return 0;
+}
