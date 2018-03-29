f9e07a9c6fe504d7725a46314f522be43692834d
徐兴军 <xxjroom@163.com>
2018-03-29 18:18:48 +0800

    一个extern的样例

---
+++ c/xuxingjun/chapter02/ExampleForExtern.h
@@ -0,0 +1,5 @@
+#ifndef EXAMPLEFOREXTERN_H
+#define EXAMPLEFOREXTERN_H
+extern int g_example_for_extern;
+//void init();
+#endif
---
+++ c/xuxingjun/chapter02/extern.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include "ExampleForExtern.h"
+
+int main() {
+	cout << "g_example_for_extern = " << g_example_for_extern << endl;
+	return 0;
+}
