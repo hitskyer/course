2cd652e0699b40776f0fd714c03f10218a9dea55
kobe24o <mingmingchen1989@163.com>
2018-04-27 01:39:03 +0800

    modfiy enum.cpp

--- c/chenmingming/chapter02/enum.cpp
+++ c/chenmingming/chapter02/enum.cpp
@@ -12,4 +12,16 @@ int main()
 	cout << "day = " << day << endl;
 	week1 myday_haha = Friday1;
 	cout << "myday is 21? right!" << myday_haha << endl;
+	week1 goodday;
+	cout << "goodday is " << goodday << endl;
+	//week1 badday = 9;  //error
+	//cout << "badday is " << badday << endl;
+	
+	week1 day1,day2;
+	cout << "day1 is " << day1 << "day2 is "<< day2 << endl;
+	week1 day3,day4=Monday1;
+	cout << "day3 is " << day3 << "day4 is "<< day4 << endl;
+	enum year { a,b=5,c};
+	cout << "a is " << a << " b is "<< b << " c is "<< c << endl;
+
 }
