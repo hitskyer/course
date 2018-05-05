213c33075856eff000b04c4040a85995655cce17
yangwenbinGit <420097009@qq.com>
2018-05-05 20:25:48 +0800

    代码提交

--- c/yangwenbin/chapter03/enum.cpp
+++ c/yangwenbin/chapter03/enum.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
+
+//理解: 枚举可以用来保存一组属性的值.比如当官中有军 师 旅 团 营 连 排 班 等几个等级，这样一组属性就可以用枚举表示
 int main() {
 	enum open_modes {input, output, append};
 	enum Forms {shape = 1, sphere, cylinder, polygon};
 	enum Points {point2d = 2, point2w,
 				 point3d = 3, point3w};
-	//定义枚举变量
+	//定义枚举变量  默认是递增的
 	Points pt = point2d;
 	cout << "pt = " << pt << endl;
 	pt = point2w;
@@ -15,6 +17,17 @@ int main() {
 	cout << "pt = " << pt << endl;
 	pt = point3w;
 	cout << "pt = " << pt << endl;
+
+	cout << "--------------------------------------------------------------" << endl;
+	Forms f =shape;
+	cout << "f = " << f << endl;
+	f= sphere;
+	cout << "f = " << f << endl;
+	f= cylinder;
+        cout << "f = " << f << endl;
+	f=polygon;
+        cout << "f = " << f << endl;
+
 	
 	return 0;
 }
--- c/yangwenbin/chapter03/typedef.cpp
+++ c/yangwenbin/chapter03/typedef.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
-
-typedef int ChineseScore;
+//typedef 他并不创建新的类型 为现有的类型添加一个同义字 现在一个类型是float类型，比如写成 float cs =0.0; 但是大家不太容易理解cs到底是什么，给这个类型使用typedef取了一个别名
+//这样的话，我们一看cs的类型是ChineseScore 这个只是一个类型的别名，真正我们再看他的实际类型是float类型
+typedef float ChineseScore;
 typedef int EnglishScore;
 typedef int MathScore;
 int main() {
-	ChineseScore cs = 99;
+	ChineseScore cs = 99.88;
 	MathScore    ms = 98;
 	EnglishScore es = 97;
 
