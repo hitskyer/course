15dd6c451cb55ab743324af854cde99e7ea297c2
kobe24o <mingmingchen1989@163.com>
2018-07-14 20:11:35 +0800

    up

--- c/chenmingming/algorithm/b.cpp
+++ c/chenmingming/algorithm/b.cpp
@@ -3,13 +3,20 @@ using namespace std;
 int main()
 {
 	int *p = new int [10];
-	//int **t = p;
 	int *a = p;
 	p++;
 	cout << p << endl << *p << endl; //<< **p ;
-	//cout << t << endl << *t << endl;
 	delete [] a;
 	p = NULL;
 	a = NULL;
+	int *arr = new int [5];
+	arr[0] = 3;
+	arr[1] = 5;
+	arr[2] = 2;
+	arr[3] = 9;
+	arr[4] = 10;
+	cout << arr[2] << endl;
+	++arr;
+	cout << arr[2] << endl;
 	return 0;
 }
