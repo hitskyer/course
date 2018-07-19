ac9c88732ea2ed3d6cbb6fc7e46cea91cee3dc6d
JKJerry <15803824671@163.com>
2018-07-19 20:32:21 +0800

    update

---
+++ c/lufuxing/chapter03/p116_2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int ia[3][4]={0,1,2,3,4,5,6,7,8,9,10,11};
+  cout<<"利用范围for语句输出多维数组的内容："<<endl;
+  for(auto &row:ia)
+  {
+   for(auto &col:row)
+     cout<<col<<" ";
+   cout<<endl;
+  }
+
+  cout<<"利用普通的for语句和下标运算符输出多维数组的内容："<<endl;
+  for(auto i = 0; i!=3;i++)
+  {
+   for (auto j=0;j!=4;j++)
+    cout<<ia[i][j]<<" ";
+   cout <<endl;
+  }
+
+  cout<< "利用普通for语句和指针输出多维数组的内容："<<endl;
+  for(auto *p=ia;p!=ia+3;p++)
+  {
+    for(auto q=*p;q!=*p+4;q++)
+      cout<<*q<<" ";
+    cout<<endl;
+  }
+  return 0;
+}
---
+++ c/lufuxing/chapter03/p128.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+  int num;
+  while(cin>>num)
+  {
+    if(num=42)
+      break;
+  }
+}
+
