194217d1322fe007eac98a65480cfa5d44fd781c
JKJerry <15803824671@163.com>
2018-07-28 16:04:48 +0800

    update

---
+++ c/lufuxing/chapter06/205.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void print(vector<int> vInt,unsigned index)
+{
+  unsigned sz=vInt.size();
+  if(!vInt.empty() && index <sz)
+  {
+   cout<<vInt[index]<<endl;
+   print(vInt,index+1);
+  }
+}
+
+int main()
+{
+  vector<int> v={1,3,5,7,9,11,13,15};
+  print(v,0);
+  return 0;
+}
