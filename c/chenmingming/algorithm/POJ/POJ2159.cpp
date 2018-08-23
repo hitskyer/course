0661ebb3f2348875183301b739f7bbeb1629a69e
kobe24o <mingmingchen1989@163.com>
2018-08-23 14:48:24 +0800

    up

--- c/chenmingming/algorithm/POJ/POJ2159.cpp
+++ c/chenmingming/algorithm/POJ/POJ2159.cpp
@@ -7,19 +7,35 @@ int main()
     int secretnum = 1;
     char word1,word2;
     vector<char> secrettext,origintext;
-    while(1)
+    while(cin.get(word1))
     {
-    	cin >> word1;
-    	if(word1 == 32)
-    		break;
-        word1 = word1 - secretnum;
-        if(word1 < 'A')
-            word1 = word1 + 26;
-        secrettext.push_back(word1);
+        if(word1 == '\n')
+            break;
+        if(word1 >= 'A' && word1 <= 'Z')
+        {
+            word1 = word1 - secretnum;
+            if(word1 < 'A')
+                word1 = word1 + 26;
+            secrettext.push_back(word1);
+        }
+        else
+        {
+            continue;
+        }
     }
+    // cin.ignore(1,'\n');
     while(cin.get(word2))
     {
-        origintext.push_back(word2);
+        if(word2 == '\n')
+            break;
+        if(word2 >= 'A' && word2 <= 'Z')
+        {
+            origintext.push_back(word2);
+        }
+        else
+        {
+            continue;
+        }
     }
     sort(secrettext.begin(),secrettext.end());
     sort(origintext.begin(),origintext.end());
