efc1b29027b8494118b81886b95309cee2295d58
RF-DYX <379992467@qq.com>
2018-09-04 15:41:16 +0800

    chapter08 practice

--- c/dongyaxing/chapter08/eg_3.cpp
+++ c/dongyaxing/chapter08/eg_3.cpp
@@ -1,24 +1,26 @@
 #include<iostream>
 #include<fstream>
-#include<string>
-
+//#include<cstdlib>      
+//#include<string>
+#include<stdexcept>  //linux下，此头文件必须引用，否则runtime_error() 提示未声明
 using namespace std;
 
-std::istream & get(istream & in)
+istream& get(istream& in)
 {
         int ival;
         while(in>>ival,!in.eof())
         {
                 if(in.bad())
-                        //throw runtime_error("to stream corrupted");
+                {        throw runtime_error("To stream corrupted");}
                 if(in.fail())
                 {
-                        std::cerr<<"bad data,try again"<<endl;
+                        cerr<<"bad data,try again"<<endl;
                         in.clear(istream::failbit);
                         in.ignore(200,' ');
-                        continue;
-                }
-                std::cout<<ival<<" ";
+                        //continue;
+                        break;
+		}
+                cout<<ival<<" ";
         }
         in.clear(istream::failbit);
 	return in;
@@ -26,22 +28,10 @@ std::istream & get(istream & in)
 
 int main()
 {
-
-//	double dval;
-//	get(cin);
-//	cin>>dval;
-//	cout<<dval<<endl;
-
-
-	string fileName;
-	cout<<"Enter file name:"<<endl;
-	cin>>fileName;
-	ifstream inFile(fileName.c_str());
-	if(!inFile)
-	{
-		cerr<<"error:can not open input file:"<<fileName<<endl;
-	}
-	get(inFile);
-
+	cout<<"只能输入整数，小数或字符串，则退出"<<endl;
+	double dval;
+	get(cin);
+	cin>>dval;
+	cout<<dval<<endl;
 	return 0;
 }
