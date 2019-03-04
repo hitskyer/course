56894809f9db99f1337d85f7b6ea9bb86deeda86
kobe_24o <mingmingchen1989@163.com>
2019-03-04 22:30:36 +0800

    test clion git

---
+++ dataAlgorithm/chenmingming/linkedList/single_linkedlist.h
@@ -0,0 +1,12 @@
+#ifndef LINKEDLIST_SINGLE_LINKEDLIST_H
+#define LINKEDLIST_SINGLE_LINKEDLIST_H
+
+#include<iostream>
+using namespace std;
+template <class T> class single_linkedlist
+{
+
+
+};
+
+#endif //LINKEDLIST_SINGLE_LINKEDLIST_H
