3366b369c6c81acffcd336a852807983304467ce
kobe_24o <mingmingchen1989@163.com>
2019-03-05 00:24:30 +0800

    single linked list update

--- dataAlgorithm/chenmingming/linkedList/main.cpp
+++ dataAlgorithm/chenmingming/linkedList/main.cpp
@@ -1,6 +1,9 @@
+#include "single_linkedlist.h"
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    Single_linkedlist<string> strlist;
     return 0;
 }
--- dataAlgorithm/chenmingming/linkedList/single_linkedlist.h
+++ dataAlgorithm/chenmingming/linkedList/single_linkedlist.h
@@ -3,10 +3,36 @@
 
 #include<iostream>
 using namespace std;
-template <class T> class single_linkedlist
+
+template <class ElemType> struct LinkNode      //节点类
 {
-
-
+    ElemType _data;    //节点的数据
+    LinkNode* _next;    //指向该节点的下一个节点的指针
+    LinkNode(): _next(NULL) {}
+    LinkNode(const ElemType &d, LinkNode* p = NULL) : _data(d), _next(p) {}
+    ~LinkNode(){}
+};
+template <class ElemType> class Single_linkedlist
+{
+    LinkNode<ElemType> *p_head, *p_tail;    //首尾指针
+    int listlength; //链表长度
+public:
+    void* get_p_head() //返回头节点的位置，即头指针
+    {
+        return p_head;
+    }
+    void* get_p_tail() //返回为节点的位置，即尾指针
+    {
+        return p_tail;
+    }
+    int getLength()  //返回链表长度
+    {
+        return listlength;
+    }
+    ElemType getCurData(LinkNode<ElemType>* p) const  //返回当前节点的数据内容
+    {
+        return p->_data;
+    }
 };
 
 #endif //LINKEDLIST_SINGLE_LINKEDLIST_H
