4e3d8f5a0d885bc8c6338f8fb25233c3a232419c
kobe_24o <mingmingchen1989@163.com>
2019-03-11 23:46:55 +0800

    poj 1250 AC

--- c/chenmingming/algorithm/POJ/poj1250.cpp
+++ c/chenmingming/algorithm/POJ/poj1250.cpp
@@ -79,34 +79,34 @@ struct list
 
 int main()
 {
-    list beds,waitlist;
+    list beds,waitlist; //床位队列，等待队列
     node *tempnode = NULL;
     size_t numsofbed, walkedaway = 0;
     char ch;
-    while(cin >> numsofbed && numsofbed)
+    while(cin >> numsofbed && numsofbed)    //输入床位数，且不为0
     {
-        cin.get();
-        while(cin.get(ch) && ch != '\n')
+        cin.get();  //拿掉空格
+        while(cin.get(ch) && ch != '\n')    //输入每个人
         {
-            tempnode = beds.find(ch);
-            if(tempnode)
+            tempnode = beds.find(ch);   //去床位队列查找人
+            if(tempnode)    //找到了这个人，床位上这个人离店
             {
                 beds.delNode(ch);
             }
-            else
+            else    //没有在床位上找到该人，则该人需要住店
             {
-                if(beds.listLength < numsofbed)
+                if(beds.listLength < numsofbed) //床位有空余
                 {
-                    beds.push_front(ch);
+                    beds.push_front(ch);    //这个人住下
                 }
-                else
+                else    //床位满了
                 {
-                    if(waitlist.find(ch))
+                    if(waitlist.find(ch))   //这个人已经在等待队列里
                     {
-                        waitlist.delNode(ch);
+                        waitlist.delNode(ch);   //等不了了，离开等待队列
                         walkedaway++;
                     }
-                    else
+                    else    //这个人不在等待队列里，可以等待
                     {
                         waitlist.push_front(ch);
                     }
