50c03650af99d7bea858752356ece1ed98e8b04b
kobe_24o <mingmingchen1989@163.com>
2019-03-14 22:29:53 +0800

    palindrome problem

--- dataAlgorithm/chenmingming/linkedList/palindrome.cpp
+++ dataAlgorithm/chenmingming/linkedList/palindrome.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-struct Node
+struct Node //节点
 {
     char data;
     Node* next;
@@ -12,16 +12,16 @@ struct Node
     Node(char &ch):data(ch),next(NULL){}
     ~Node(){}
 };
-class SLinkedList
+class SLinkedList   //链表
 {
     Node* p_head;
 public:
-    SLinkedList()
+    SLinkedList()   //构造函数
     {
-        p_head = new Node;
+        p_head = new Node;  //带头链表
 //        cout << "new 1" << endl;
     }
-    ~SLinkedList(){ erase(); }
+    ~SLinkedList(){ erase(); }  //析构函数
     void erase()
     {
         Node *del_tempNode, *tempNode;
@@ -42,7 +42,7 @@ public:
     {
         return p_head;
     }
-    size_t get_len()
+    size_t get_len()    //求链表长度
     {
         size_t len = 0;
         Node* p = p_head;
@@ -53,14 +53,14 @@ public:
         }
         return len;
     }
-    void delHeadSentinel()
+    void delHeadSentinel()  //删除链表表头哨兵
     {
         Node* del = p_head;
         p_head = p_head->next;
         delete del; //删除链表的表头哨兵
 //        cout << "delete head 1" << endl;
     }
-    Node* reverse()
+    Node* reverse() //链表反转
     {
         if(p_head == NULL || p_head->next == NULL)
             return NULL;
@@ -81,7 +81,7 @@ public:
             return p_head;
         }
     }
-    Node* findMiddle()
+    Node* findMiddle()  //查找链表中点
     {
         size_t len = get_len();
         Node* tempNode = p_head;
@@ -107,9 +107,9 @@ int main()
             cout << "empty word !" << endl;
             continue;
         }
-        SLinkedList charList, backHalfOfList;
+        SLinkedList charList, backHalfOfList;   //链表(前半部分链表)，后半部分链表
         Node* tempNode = charList.get_head();
-        while(ch != '\n')   //把单词存进链表
+        while(ch != '\n')   //把单词存进链表charList
         {
             Node* newNode = new Node(ch);
 //            cout << "new insert 1" << endl;
@@ -117,22 +117,22 @@ int main()
             tempNode = newNode;
             ch = cin.get();
         }
-        charList.delHeadSentinel();
+        charList.delHeadSentinel(); //链表表头删除
         backHalfOfList.delHeadSentinel();   //把空表头哨兵节点删除
-        Node* endOfFrontList = charList.findMiddle();
-        Node* backListHead = endOfFrontList->next;
-        endOfFrontList->next = NULL;
-        backHalfOfList.set_head(backListHead);
-        backHalfOfList.reverse();
-        size_t n = backHalfOfList.get_len();
-        Node *frontList = charList.get_head();
-        Node *backList = backHalfOfList.get_head();
+        Node* endOfFrontList = charList.findMiddle();//链表的中点是前一半的结束节点
+        Node* backListHead = endOfFrontList->next;//中点的下一个节点是后半部分的开始
+        endOfFrontList->next = NULL;//把前半部分链表断开
+        backHalfOfList.set_head(backListHead);//把后半部分的链表表头地址设置好
+        backHalfOfList.reverse();//后半部分链表反转
+        size_t n = backHalfOfList.get_len();//求后半部分链表长度
+        Node *frontList = charList.get_head();//找到前半部分的开头
+        Node *backList = backHalfOfList.get_head();//后半部分的开头（反转后的）
         bool answer = false;
-        if(backList == NULL)
+        if(backList == NULL)    //如果后半部分为空，说明只有一个字符
             answer = true;
         else
         {
-            for(size_t i = 0; i < n; ++i)
+            for(size_t i = 0; i < n; ++i)//比较数据是否相同
             {
                 if(frontList->data != backList->data)
                 {
