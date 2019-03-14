387edbe51733ce4e3290464682c6153598e9776c
kobe_24o <mingmingchen1989@163.com>
2019-03-14 22:53:22 +0800

    约瑟夫环问题，添加注释

--- dataAlgorithm/chenmingming/linkedList/Jose.cpp
+++ dataAlgorithm/chenmingming/linkedList/Jose.cpp
@@ -14,7 +14,7 @@ private:
 public:
     Jose()
     {
-        p_head = new NodeType;
+        p_head = new NodeType;  //带空头的链表
         p_head->next = p_head;  //空的循环链表
     }
     ~Jose(){}
@@ -35,11 +35,11 @@ void Jose::creat()
         cout << "\n enter name: ";
         cin >> newp->name;
         newp->next = p_head;    //此处p_head为尾部哨兵
-        tempNode->next = newp;   //不断地往尾部插入节点
+        tempNode->next = newp;   //不断地往尾部（尾部哨兵之前）插入节点
         tempNode = newp;
     }
-    tempNode->next = p_head->next;
-    delete p_head;  //删除哨兵节点
+    tempNode->next = p_head->next;  //断开空头哨兵
+    delete p_head;  //释放哨兵节点
     p_head = tempNode->next;    //头结点指向第一个节点
 }
 void Jose::print()
@@ -49,27 +49,27 @@ void Jose::print()
     cout << "\n enter value m(m>=2):";
     cin >> m;
     cout << "\n start:" << endl;
-    while(del->next != del)
+    while(del->next != del) //链表节点个数不为1
     {
-        for(i = 1; i < m; ++i)
+        for(i = 1; i < m; ++i)  //del往后移动m位
         {
             tempNode = del;
             del = del->next;
         }
         cout << del->num << " " << del->name << endl;
-        tempNode->next = del->next;
-        delete del;
+        tempNode->next = del->next; //断开del节点
+        delete del; //释放del节点
         del = tempNode->next;
     }
     cout << del->num << " " << del->name << endl;
-    delete del;
+    delete del; //链表只剩一个节点直接删除
 }
 
 int main()
 {
-    Jose game;
-    game.creat();
-    game.print();
+    Jose gameList;
+    gameList.creat();
+    gameList.print();
     cout << "press any key to exit!" ;
     cin.get();
     return 0;
