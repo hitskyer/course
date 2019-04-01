e4e26180c3af8dbf45cb51ae5e876984178cb4d8
kobe_24o <mingmingchen1989@163.com>
2019-04-01 20:19:48 +0800

    poj 1363

---
+++ dataAlgorithm/chenmingming/stack/poj1363.cpp
@@ -0,0 +1,57 @@
+// http://poj.org/problem?id=1363
+// Created by mingm on 2019/4/1.
+//
+#include <stack>
+#include <iostream>
+#define N 1000
+using namespace std;
+int main()
+{
+    int Barr[N], i = 0, j = 0, len;
+    while(cin >> len && len)
+    {
+        while (1)
+        {
+            stack<int> station;
+            for(i = 0; i < len; ++i)
+            {
+                cin >> Barr[i];
+                if(Barr[0] == 0)
+                    break;
+            }
+            if(Barr[0] == 0)
+            {
+                cout << endl;
+                break;
+            }
+            for(i = 1, j = 0; i <= len; ++i)
+            {
+                if(i == Barr[j])
+                {
+                    j++;
+                    continue;
+                }
+                else
+                {
+                    if(!station.empty() && station.top() == Barr[j])
+                    {
+                        station.pop();
+                        j++;
+                        continue;
+                    }
+                    else
+                    {
+                        station.push(i);
+                    }
+                }
+            }
+            while(!station.empty() && station.top() == Barr[j])
+                station.pop();
+            if(station.empty())
+                cout << "Yes" << endl;
+            else
+                cout << "No" << endl;
+        }
+    }
+    return 0;
+}
