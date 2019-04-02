72a459246f8fa6808b70735fca234a70e2ca2092
kobe_24o <mingmingchen1989@163.com>
2019-04-02 13:18:12 +0800

    template array Queue

--- dataAlgorithm/chenmingming/queue/arrqueue.cpp
+++ dataAlgorithm/chenmingming/queue/arrqueue.cpp
@@ -1,8 +1,12 @@
 /**
- * @description:
+ * @description: 顺序队列实现文件
  * @author: michael ming
- * @date: 2019/4/1 22:13
+ * @date: 2019/4/2 21:26
  * @modified by:
  */
-
 #include "arrqueue.h"
+template <class T>
+arrQueue<T>::arrQueue(const int capa)
+{
+
+}
--- dataAlgorithm/chenmingming/queue/arrqueue.h
+++ dataAlgorithm/chenmingming/queue/arrqueue.h
@@ -1,11 +1,31 @@
 /**
- * @description:
+ * @description: 顺序队列，内部基于数组
  * @author: michael ming
- * @date: 2019/4/1 22:13
+ * @date: 2019/4/2 21:24
  * @modified by:
  */
+typedef unsigned int UINT;
+template <class T>
+class arrQueue
+{
+public:
+    arrQueue(const int capa = 10);
+    ~arrQueue(void);
+    bool enqueue(const T& data);  //入队    从队列的尾部插入数据
+    bool dequeue();		//出队   从队列的头部弹出数据
+    UINT getlength() const;	//获得队列的长度
+    bool empty() const;		//判断队列是否为空
+    void erase();		//清空队列
+    void print() const;       //打印队列
+    const T& getHead() const;    //获取队首
+    const T& getTail() const;    //获取队尾
+private:
+    int m_pHead;
+    int m_pTail;
+    UINT m_QueueLen;
+    UINT m_capacity;
+    T* arrQueue;
+};
 
-#ifndef QUEUE_ARRQUEUE_H
-#define QUEUE_ARRQUEUE_H
-
+#include "arrqueue.cpp"
 #endif //QUEUE_ARRQUEUE_H
