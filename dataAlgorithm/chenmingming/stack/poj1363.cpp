bda3853032181f7b43429f5b3d1d22e1b1e6de2c
kobe_24o <mingmingchen1989@163.com>
2019-04-03 18:14:43 +0800

    poj 1363 add comment

--- dataAlgorithm/chenmingming/stack/poj1363.cpp
+++ dataAlgorithm/chenmingming/stack/poj1363.cpp
@@ -5,36 +5,36 @@ using namespace std;
 int main()
 {
     int Barr[N], i = 0, j = 0, len;
-    while(cin >> len && len)
+    while(cin >> len && len)    //输入火车长度
     {
         while (1)
         {
             stack<int> station;
-            for(i = 0; i < len; ++i)
+            for(i = 0; i < len; ++i)    //输入火车B站的车厢顺序
             {
-                cin >> Barr[i];
-                if(Barr[0] == 0)
+                cin >> Barr[i]; 
+                if(Barr[0] == 0)    //第一个车厢是0，结束
                     break;
             }
-            if(Barr[0] == 0)
+            if(Barr[0] == 0)    //第一个车厢是0，结束
             {
                 break;
             }
-            for(i = 1, j = 0; i <= len; ++i)
+            for(i = 1, j = 0; i <= len; ++i)    //对1-N的车厢进行操作
             {
-                while(!station.empty() && station.top() == Barr[j])
+                while(!station.empty() && station.top() == Barr[j]) //中转站（栈）不为空则，比较栈顶与B的车厢的号码
                 {
-                    station.pop();
-                    j++;
+                    station.pop();  //相等的话，车厢出栈
+                    j++;    //可以比较下一个车厢号码了
                 }
-                station.push(i);
+                station.push(i);    //栈为空或者栈顶车厢号与要求的不等，则直接把A站的车压入栈内
             }
-            while(!station.empty() && station.top() == Barr[j])
+            while(!station.empty() && station.top() == Barr[j]) //当1-N都压栈完了，栈内还有车厢的话，顺次和栈顶比较
             {
                 station.pop();
                 j++;
             }
-            if(station.empty())
+            if(station.empty()) //最后栈内为空则，可以匹配要求的顺序
                 cout << "Yes" << endl;
             else
                 cout << "No" << endl;
