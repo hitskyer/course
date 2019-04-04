d2413a1b870186912a5c54eded16ffb21598d8b9
kobe_24o <mingmingchen1989@163.com>
2019-04-04 22:59:13 +0800

    Update YangHuiTriangle.cpp

--- dataAlgorithm/chenmingming/queue/YangHuiTriangle.cpp
+++ dataAlgorithm/chenmingming/queue/YangHuiTriangle.cpp
@@ -5,32 +5,36 @@
  * @modified by: 
  */
 #include <iostream>
-#include "listQueue.h"
+#include <queue>
+#include <iomanip>
+using  namespace std;
 void yanghuitriangle(size_t n)
 {
-    ListQueue<int> line;
-    line.enqueue(1);    //第一行元素
-    int temp;
+    queue<int> line;
+    line.push(1);    //第一行元素
+    int temp, space = n;
     for(int i = 2; i <= n+1; ++i)   //打印第n行元素，产生第n+1行队列
     {
-        line.enqueue(1);        //第n+1行的第一个元素
+        line.push(1);        //第n+1行的第一个元素
+        cout << setw(space--);  //设置开始打印前的空格个数
         for(int j = 1; j <= i - 2; ++j) //产生第n+1行的中间n-1个元素
         {
-            temp = line.getHead()->data;
+            temp = line.front();
             cout << temp << " ";    //打印第n行的元素，并出队
-            line.dequeue();
-            temp = temp + line.getHead()->data; //相邻两数相加，并入队（n+1行的）
-            line.enqueue(temp);
+            line.pop();
+            temp = temp + line.front(); //相邻两数相加，并入队（n+1行的）
+            line.push(temp);
         }
-        cout << line.getHead()->data << endl;   //打印第n行最后一个元素，并出队
-        line.dequeue();
-        line.enqueue(1);    //n+1行最后1个元素
+        cout << line.front() << endl;   //打印第n行最后一个元素，并出队
+        line.pop();
+        line.push(1);    //n+1行最后1个元素
     }
 }
 
 int main()
 {
     size_t N;
+    cout << "请输入杨辉三角的行数：";
     cin >> N;
     yanghuitriangle(N);
     return 0;
