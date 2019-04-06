db525f5bc409a09bcb80a4f27aba90ff2a8bd239
kobe_24o <mingmingchen1989@163.com>
2019-04-06 15:05:55 +0800

    step problem recursion

--- dataAlgorithm/chenmingming/recursion/howManyWayToGo.cpp
+++ dataAlgorithm/chenmingming/recursion/howManyWayToGo.cpp
@@ -1,15 +1,66 @@
 #include <iostream>
+#include <iterator>
+//#include <utility>
+#include <map>
 using namespace std;
-int cal(int n)
+size_t cal(size_t n, map<size_t, size_t>& n_fn_map)
 {
-	if(n=1)
-		return 1;
-	if(n=2)
-		return 2;
-	return cal(n-1)+cal(n-2);
+    map<size_t,size_t>::iterator iter = n_fn_map.find(n);
+    if(iter != n_fn_map.end())
+    {
+        return iter->second;
+    }
+	if(n==1)
+    {
+        n_fn_map.insert(pair<size_t, size_t>(1,1));
+	    return 1;
+    }
+	else if(n==2)
+    {
+        n_fn_map.insert(pair<size_t, size_t>(2,2));
+	    return 2;
+    }
+    else
+    {
+        size_t sum = cal(n-1,n_fn_map)+cal(n-2,n_fn_map);
+        n_fn_map.insert(n,sum);
+        return sum;
+    }
 }
-int main()
+int main()  //递归(带避免重复计算fn的值功能)
 {
-	cout << cal(39);
+    size_t n;
+    cout << "请输入你要走的台阶数 n :" ;
+    cin >> n;
+    map<size_t, size_t> n_Fn;
+	cout << "走台阶有 " << cal(n,n_Fn) << " 种方案。" << endl;
 	return 0;
 }
+
+//int main()  //循环
+//{
+//    size_t n, step, nextStep = 2, nextnextStep = 1;
+//    cout << "请输入你要走的台阶数 n :" ;
+//    cin >> n;
+//    if(n > 0)
+//    {
+//        if(n == 1)
+//        {
+//            step = 1;
+//        } else if(n == 2)
+//        {
+//            step = 2;
+//        }
+//        else
+//        {
+//            for(int i = 2; i < n; ++i)
+//            {
+//                step = nextStep + nextnextStep;
+//                nextnextStep = nextStep;
+//                nextStep = step;
+//            }
+//        }
+//	    cout << "走台阶有 " << step << " 种方案。" << endl;
+//    }
+//	return 0;
+//}
