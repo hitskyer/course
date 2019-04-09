170b6aafc1456d1e0331820d41a74015961a70f6
kobe24o <mingmingchen1989@163.com>
2019-04-09 16:34:44 +0800

    update

--- python/chenmingming/file_error/pi_print.py
+++ python/chenmingming/file_error/pi_print.py
@@ -1,4 +1,13 @@
 filename = 'pi_digits.txt'
 with open(filename) as pi_file: #with帮助我们适时关闭文件
-    for line_content in pi_file:
-        print(line_content.rstrip())
+    lines = pi_file.readlines()
+pi_str = ''
+for line in lines:
+    pi_str += line.strip()
+print(pi_str[:7]+"...")
+print(len(pi_str))
+birthday = input("输入你的生日：yyyymmdd ")
+if birthday in pi_str:
+    print("你的生日出现在pi中。")
+else:
+    print("你的生日不在pi中。")
