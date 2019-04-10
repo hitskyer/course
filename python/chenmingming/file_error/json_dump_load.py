661a01edbaceca1f8378712531d051d177cde863
kobe_24o <mingmingchen1989@163.com>
2019-04-10 20:07:29 +0800

    json

--- python/chenmingming/file_error/json_dump_load.py
+++ python/chenmingming/file_error/json_dump_load.py
@@ -7,3 +7,30 @@ with open(filename,'w') as file:
 with open(filename) as file:
     numbers = json.load(file)
 print(numbers)
+
+def get_stored_username():
+    filename = "username.json"
+    try:
+        with open(filename) as file:
+            username = json.load(file)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    username = input("What is your name? ")
+    filename = "username.json"
+    with open(filename, 'a') as file:
+        json.dump(username, file)
+    return username
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        print("Welcome back, ", username, " !")
+    else:
+        get_new_username()
+        print("We'll remember you when you come back, ", username, " !")
+
+greet_user()
