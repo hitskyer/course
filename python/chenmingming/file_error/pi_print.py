fc49c879636c3a1e183a1eb39d76f464ef088a2a
kobe_24o <mingmingchen1989@163.com>
2019-04-11 20:04:27 +0800

    update

--- python/chenmingming/file_error/pi_print.py
+++ python/chenmingming/file_error/pi_print.py
@@ -1,9 +1,9 @@
 filename = 'pi_digits.txt'
 with open(filename) as pi_file: #with帮助我们适时关闭文件
-    lines = pi_file.readlines()
+    lines = pi_file.readlines()	#把文件按行存储
 pi_str = ''
 for line in lines:
-    pi_str += line.strip()
+    pi_str += line.strip()	#strip()行左右的空删除
 print(pi_str[:7]+"...")
 print(len(pi_str))
 birthday = input("输入你的生日：yyyymmdd ")
