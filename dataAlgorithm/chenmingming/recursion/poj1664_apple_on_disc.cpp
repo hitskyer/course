7e1f4908f1cb83e0995da4bceae9cb1ae0ea642c
kobe24o <mingmingchen1989@163.com>
2019-04-15 13:29:14 +0800

    poj 1664 apple on disc

---
+++ dataAlgorithm/chenmingming/recursion/poj1664_apple_on_disc.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+unsigned long func(size_t apple, size_t disc)
+{
+    
+}
+int main()
+{
+    size_t t, apple, disc;
+    cin >> t;
+    while(t--)
+    {
+        cin >> apple >> disc;
+        cout << func(apple, disc) << endl;
+    }
+}
