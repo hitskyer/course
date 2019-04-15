5d29e748833544297de2b054e92c481ae3716a19
kobe24o <mingmingchen1989@163.com>
2019-04-15 14:08:38 +0800

    poj 1664 apple on disc

--- dataAlgorithm/chenmingming/recursion/poj1664_apple_on_disc.cpp
+++ dataAlgorithm/chenmingming/recursion/poj1664_apple_on_disc.cpp
@@ -2,15 +2,25 @@
 using namespace std;
 unsigned long func(size_t apple, size_t disc)
 {
-    
+    if(apple == 1 || apple == 0)
+        return 1;
+    if(disc == 1)
+        return 1;
+    if(apple < disc)
+        return func(apple, apple);
+    else
+        return func(apple, disc-1) + func(apple-disc, disc);
 }
 int main()
 {
     size_t t, apple, disc;
+    unsigned long sum = 0;
     cin >> t;
     while(t--)
     {
         cin >> apple >> disc;
-        cout << func(apple, disc) << endl;
+        sum = func(apple, disc);
+        cout << sum << endl;
     }
+    return 0;
 }
