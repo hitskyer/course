a93d31b5d1620642a6ba435fc0191a7026d039fb
kobe_24o <mingmingchen1989@163.com>
2019-04-22 23:25:06 +0800

    skiplist

---
+++ dataAlgorithm/chenmingming/skiplist/skiplist.cpp
@@ -0,0 +1,8 @@
+/**
+ * @description: 
+ * @author: michael ming
+ * @date: 2019/4/22 22:21
+ * @modified by: 
+ */
+
+#include "skiplist.h"
---
+++ dataAlgorithm/chenmingming/skiplist/skiplist.h
@@ -0,0 +1,31 @@
+/**
+ * @description: 跳表
+ * @author: michael ming
+ * @date: 2019/4/22 22:21
+ * @modified by: 
+ */
+
+#ifndef SKIPLIST_SKIPLIST_H
+#define SKIPLIST_SKIPLIST_H
+typedef unsigned int UINT
+template <class T>
+class skipNode<T>
+{
+public:
+    T data;
+    skipNode<T> **next; //跳表节点的next是 skipNode<T>* 类型的数组
+    skipNode(const T& inputdata, UINT level):data(inputdata)
+    {
+        next = new skipNode<T>* [level+1];  //索引级别从0（链表自身）开始
+    }
+};
+template <class T>
+class skiplist<T>
+{
+public:
+    UINT level;
+    skipNode<T> *head;
+};
+
+
+#endif //SKIPLIST_SKIPLIST_H
