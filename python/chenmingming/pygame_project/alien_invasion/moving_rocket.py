823a2f1a9d8abe6e14507d206bd620fcadcaf88d
kobe24o <mingmingchen1989@163.com>
2019-04-24 17:27:59 +0800

    Update moving_rocket.py

--- python/chenmingming/pygame_project/alien_invasion/moving_rocket.py
+++ python/chenmingming/pygame_project/alien_invasion/moving_rocket.py
@@ -3,7 +3,14 @@ import pygame
 from rocket_settings import RocketSettings
 from rocket import Rocket
 import rocket_functions as rf
-def 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("测试小火箭")
+def go_rocket():
+    pygame.init()
+    game_setting = RocketSettings()
+    screen = pygame.display.set_mode((game_setting.screen_width, game_setting.screen_height))
+    pygame.display.set_caption("测试小火箭")
+    rocket = Rocket(game_setting, screen)
+    while True:
+        rf.check_events(rocket)
+        rocket.update()
+        rf.update_screen(game_setting, screen, rocket)
+go_rocket()
