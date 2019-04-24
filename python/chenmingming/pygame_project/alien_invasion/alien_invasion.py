638454943bd1849b75646b085c3070c8fd1eca0d
kobe24o <mingmingchen1989@163.com>
2019-04-24 13:43:28 +0800

    creat Alien game

---
+++ python/chenmingming/pygame_project/alien_invasion/alien_invasion.py
@@ -0,0 +1,9 @@
+import sys
+import pygame
+def run_game():
+    pygame.init()
+    pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
+    while True:
+        screen.fill(bg_color)
+        
