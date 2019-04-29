c5e78cebbf657d4656c0e962a1388c38e4731aed
kobe_24o <mingmingchen1989@163.com>
2019-04-29 17:53:10 +0800

    Update skiplist.h

--- dataAlgorithm/chenmingming/skiplist/skiplist.h
+++ dataAlgorithm/chenmingming/skiplist/skiplist.h
@@ -7,6 +7,9 @@
 
 #ifndef SKIPLIST_SKIPLIST_H
 #define SKIPLIST_SKIPLIST_H
+#include <ctime>
+#include <cstdlib>
+
 typedef unsigned int UINT;
 template <class T>
 class skipNode
@@ -14,19 +17,42 @@ class skipNode
 public:
     T data;
     skipNode<T> **next; //跳表节点的next是 skipNode<T>* 类型的数组
-    skipNode(const T& inputdata, UINT level):data(inputdata)
+    skipNode(UINT level)
     {
         next = new skipNode<T>* [level+1];  //索引级别从0（链表自身）开始
+        for(int i = 0; i < level+1; ++i)
+            next[i] = NULL;
+    }
+    skipNode(UINT level, const T& inputdata):data(inputdata)
+    {
+        next = new skipNode<T>* [level+1];  //索引级别从0（链表自身）开始
+        for(int i = 0; i < level+1; ++i)
+            next[i] = NULL;
     }
 };
 template <class T>
 class skiplist
 {
+private:
+    UINT randomLevel()
+    {
+        UINT lv = 0;
+        while(rand()%2)
+            lv++;
+        return lv > maxLevel ? maxLevel:lv;
+    }
 public:
-    UINT level;
+    UINT maxLevel;
     skipNode<T> *head;
-//    skiplist<T>
-};
+    skiplist<T>(UINT level = 10):maxLevel(level)
+    {
+        head = new skipNode<T>();
+    }
+    void insert(const T& inputdata)
+    {
+        skipNode<T>* newNode = new skipNode<T>(maxLevel, inputdata);
 
+    }
+};
 
 #endif //SKIPLIST_SKIPLIST_H
