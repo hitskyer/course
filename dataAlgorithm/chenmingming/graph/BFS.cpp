28eb4c572716793338ecbd53ad51d78b1fe92fd1
kobe_24o <mingmingchen1989@163.com>
2019-06-04 23:46:22 +0800

    graph

---
+++ dataAlgorithm/chenmingming/graph/BFS.cpp
@@ -0,0 +1,84 @@
+/**
+ * @description: BFS
+ * @author: michael ming
+ * @date: 2019/6/4 23:05
+ * @modified by: 
+ */
+#include <list>
+#include <queue>
+#include <iostream>
+#include <memory.h>
+using namespace std;
+class graph
+{
+    int v;//顶点个数
+    list<int> *adj;
+public:
+    graph(int numofvertex)
+    {
+        v = numofvertex;
+        adj = new list<int> [v];
+    }
+    ~graph()
+    {
+        delete [] adj;
+    }
+    void insertEdge(int s, int t)
+    {
+        s--;t--;
+        adj[s].push_back(t);
+    }
+    void print()
+    {
+        for(int i = 0; i < v; ++i)
+        {
+            cout << "邻接表，节点 " << i << " is \n";
+            for(auto it = adj[i].begin(); it != adj[i].end();++it)
+            {
+                cout << *it << " ";
+            }
+            cout << endl;
+        }
+    }
+    void bfs(int s)
+    {
+        bool *visited = new bool [v+1];
+        memset(visited,false, sizeof(bool)*(v+1));
+        visited[s] = true;
+        list<int> q;
+        q.push_back(s);
+        list<int>::iterator it;
+        while(!q.empty())
+        {
+            int w = q.front();
+            cout << w << " ";
+            q.pop_front();
+            for(it = adj[w].begin(); it != adj[w].end();++it)
+            {
+                if(visited[*it]==false)
+                {
+                    visited[*it] = true;
+                    q.push_back(*it);
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    graph gp(8);
+    gp.insertEdge(1,2);
+    gp.insertEdge(2,3);
+    gp.insertEdge(1,4);
+    gp.insertEdge(2,5);
+    gp.insertEdge(3,6);
+    gp.insertEdge(4,5);
+    gp.insertEdge(5,6);
+    gp.insertEdge(5,7);
+    gp.insertEdge(6,8);
+    gp.insertEdge(7,8);
+    gp.print();
+    gp.bfs(2);
+    return 0;
+}
