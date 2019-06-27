3181201d71373177909fb887d4fc71ea17f73612
kobe24o <mingmingchen1989@163.com>
2019-06-27 16:34:56 +0800

    Update poj3690_2d_match.cpp

--- dataAlgorithm/chenmingming/string_matching/poj3690_2d_match.cpp
+++ dataAlgorithm/chenmingming/string_matching/poj3690_2d_match.cpp
@@ -10,7 +10,6 @@ typedef unsigned long long ull;
 int a[1001][1001];
 int b[51][51];
 ull hash_b_h[51];//存放每行的哈希值，每行相当于一个2进制数
-ull hash_b_v[51];//存放每列的哈希值，每行相当于一个2进制数
 ull hash_a_h[1001][1001];//存放主串子串行的哈希值
 ull hash_a_v[1001][1001];//存放主串子串列的哈希值
 void cal_hash_b_h(int r, int c, int b[][51])
@@ -20,7 +19,7 @@ void cal_hash_b_h(int r, int c, int b[][51])
     for (i = 0; i < r; ++i) //计算2d模式串的hash值value
     {
         value = 0;
-        for(j = 0, k = r-1; j < c; ++j,--k)
+        for(j = 0, k = c-1; j < c; ++j,--k)
         {
             value += b[i][j]<<k;
         }
@@ -28,20 +27,15 @@ void cal_hash_b_h(int r, int c, int b[][51])
     }
     return;
 }
-void cal_hash_b_v(int r, int c, int b[][51])
+ull cal_hash_b_v(int r, int c, int b[][51])
 {
-    int i, j, k;
-    ull value;
-    for (j = 0; j < c; ++j) //计算2d模式串的hash值value
+    int i, k;
+    ull value = 0;
+    for(i = 0, k = r-1; i < r; ++i,--k)
     {
-        value = 0;
-        for(i = 0, k = c-1; i < r; ++i,--k)
-        {
-            value += b[i][j]<<k;
-        }
-        hash_b_h[i] = value;
+        value += hash_b_h[i]<<k;
     }
-    return;
+    return value;
 }
 void cal_hash_a_child_h(int N, int M, int a[][1001], int P, int Q)
 {
@@ -63,27 +57,39 @@ void cal_hash_a_child_h(int N, int M, int a[][1001], int P, int Q)
         }
     }
 }
+void cal_hash_a_child_v(int N, int M, int a[][1001], int P, int Q)
+{
+    int i, j, k, x;
+    ull hash_value;
+    for (j = 0; j < M-Q+1; ++j) //计算2d子串的每行的hash值
+    {
+        for(i = 0; i < N-P+1; ++i)
+        {
+            if(i == 0)
+            {
+                hash_value = 0;
+                for(x = i, k = P-1; x < i+P && k >= 0; ++x,--k)
+                    hash_value += hash_a_h[x][j]<<k;
+            }
+            else
+                hash_value = ((hash_a_v[i-1][j]-(hash_a_h[i-1][j]<<(P-1)))<<1) + hash_a_h[i+P-1][j];
+            hash_a_v[i][j] = hash_value;
+        }
+    }
+}
 
 int str_RK_2d(int a[][1001], int N, int M, int b[][51], int P, int Q)//s是主串，t是模式串
 {
-    int i, j, k, x;
-    bool flag = false;
+    int i, j;
+//    bool flag = false;
+    ull hashb;
     cal_hash_b_h(P,Q,b);//计算2d模式串每行哈希值
+    hashb = cal_hash_b_v(P,Q,b);
     for(j = 0; j < M-Q+1; ++j)//列最多nc-mc+1次比较,分别比较每行，列先固定
     {
         for(i = 0; i < N-P+1; ++i)
         {//行最多nr-mr+1次比较
-            for(x = i, k = 0; x < i+P && k < P; ++x,++k)
-            {//一组比较P行
-                if(hash_a_h[x][j] == hash_b_h[k])//比较子串哈希值
-                    flag = true;
-                else
-                {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag == true)
+            if(hash_a_v[i][j] == hashb)//比较子串哈希值
                 return 1;
         }
     }
@@ -131,6 +137,7 @@ int main()
         count = 0;
         creatMatrix_a(a,N,M);
         cal_hash_a_child_h(N,M,a,P,Q);
+        cal_hash_a_child_v(N,M,a,P,Q);
         while(T--)
         {
             creatMatrix_b(b,P,Q);
