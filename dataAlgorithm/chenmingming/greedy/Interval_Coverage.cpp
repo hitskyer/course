7c1891f122d93803145e8fb6c54de63ac0523007
kobe_24o <mingmingchen1989@163.com>
2019-06-30 00:20:00 +0800

    Update Interval_Coverage.cpp

--- dataAlgorithm/chenmingming/greedy/Interval_Coverage.cpp
+++ dataAlgorithm/chenmingming/greedy/Interval_Coverage.cpp
@@ -5,4 +5,29 @@
  * @modified by: 
  */
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
+struct interval
+{
+    int left, right;
+};
+bool comp(interval &a, interval &b)
+{
+    if(a.left < b.left)
+        return true;
+    if(a.left = b.left && a.right < b.right)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    interval qujian[6] = {{2,4},{3,5},{5,9},{6,8},{8,10},{1,5}};
+    sort(qujian,qujian+5,comp);
+    for(int i = 0; i < 5; ++i)
+    {
+
+    }
+    return 0;
+}
