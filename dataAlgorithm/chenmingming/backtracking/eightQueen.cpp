1dca3f4ad2abcf4e2c39cfeb50a737b3111ace26
kobe_24o <mingmingchen1989@163.com>
2019-07-07 00:21:40 +0800

    Create eightQueen.cpp

---
+++ dataAlgorithm/chenmingming/backtracking/eightQueen.cpp
@@ -0,0 +1,12 @@
+/**
+ * @description: 回溯算法--八皇后问题
+ * @author: michael ming
+ * @date: 2019/7/7 0:10
+ * @modified by: 
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "";
+}
