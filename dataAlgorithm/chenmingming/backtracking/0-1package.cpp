adde9e665011a6cda787415cc6d16104f02ba1f3
kobe_24o <mingmingchen1989@163.com>
2019-07-10 23:43:21 +0800

    Update 0-1package.cpp

--- dataAlgorithm/chenmingming/backtracking/0-1package.cpp
+++ dataAlgorithm/chenmingming/backtracking/0-1package.cpp
@@ -5,11 +5,11 @@
  * @modified by: 
  */
 #include <iostream>
-#define MaxWeight 51   //背包承载极限
+#define MaxWeight 76   //背包承载极限
 using namespace std;
 void fill(int i, int curWeight, int *bag, int N, int &maxweightinbag)
 {
-    if(curWeight == MaxWeight || i == N)//
+    if(curWeight == MaxWeight || i == N)//到达极限了，或者考察完所有物品了
     {
         if(curWeight > maxweightinbag)
             maxweightinbag = curWeight;//记录历史最大装载量
@@ -17,14 +17,15 @@ void fill(int i, int curWeight, int *bag, int N, int &maxweightinbag)
     }
     fill(i+1,curWeight,bag,N,maxweightinbag);
     if(curWeight+bag[i] <= MaxWeight)
-    {
+    {//没有达到极限，继续装
         fill(i+1,curWeight+bag[i],bag,N,maxweightinbag);
     }
 }
 int main()
 {
-    const int N = 10;
-    int bag[N] = {15,40,6,11,4,7,9,13,45,10};
+    const int N = 4;
+    int bag[N] = {15,6,40,21};
+//    int bag[N] = {1,2,3,4};
     int maxweightinbag = 0;
     fill(0,0,bag,N,maxweightinbag);
     cout << "最大可装进背包的重量是：" << maxweightinbag;
