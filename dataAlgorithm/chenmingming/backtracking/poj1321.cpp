979e343ddf8070776ae182b146fdee33d4f59377
kobe_24o <mingmingchen1989@163.com>
2019-07-13 17:40:44 +0800

    Update poj1321.cpp

--- dataAlgorithm/chenmingming/backtracking/poj1321.cpp
+++ dataAlgorithm/chenmingming/backtracking/poj1321.cpp
@@ -4,18 +4,17 @@
  * @date: 2019/7/13 13:58
  * @modified by: 
  */
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-bool a[10][10];
-int result[10];
-bool isok(int r, int c)//判断是否都是同种颜色
+bool a[10][10];//存储棋盘
+int result[10];//存储是否存放棋子（和8皇后类似）
+bool isok(int r, int c)//判断是否可以摆放
 {
     for(int i = r - 1; i >= 0; --i)//逐行向上考察每一行
     {
-        if(result[i] == c)//第i行的c列有棋子吗
+        if(result[i] == c)//上面所有行的c列有棋子吗
             return false;
     }
     return true;
@@ -23,29 +22,24 @@ bool isok(int r, int c)//判断是否都是同种颜色
 
 void flip(int r, int num, int &count, int n, int k)
 {
-    if(num == k)    //k个棋子都放置好了，打印结果
+    if(num == k)    //k个棋子都放置好了
     {
-        count++;
+        count++;    //记录可行方案数
         return;//都放置好了，没法再递归了
     }
     if(r == n)
         return;//边界，没法再递归了
-    for(int column = 0; column < n; ++column)//每行有n种放法
+    for(int column = 0; column < n; ++column)//r行可能有n种放法
     {
         if(a[r][column] != 1)
-            continue;
-        if(column == n - 1 && a[r][column] != 1)
-        {
-            column = 0;
-            r = r + 1;
-        }
+            continue;//寻找可放置的位置
         if(isok(r,column))    //该放法满足要求
         {
             result[r] = column;//第r行的棋子放到了column列
-            num++;
-            flip(r+1, num, count, n, k);  //考察下一行
-            result[r] = -1;
-            num--;
+            num++;//放置棋子数加1
+            flip(r+1, num, count, n, k);  //放了棋子 考察下一行
+            result[r] = -1;//恢复现场，这个地方不放棋子，找下一种方法
+            num--;//放置的棋子数减1
         }
     }
     flip(r+1, num, count, n, k);  //第r行不放棋子，考察下一行
@@ -56,8 +50,8 @@ int main()
     int i, j, n, k, num = 0, count;
     while(cin >> n >> k && n != -1)
     {
-        count = 0;
-        for (i = 0; i < n; ++i)
+        count = 0;//计数清零
+        for (i = 0; i < n; ++i)//输入地图
         {
             cin >> s;
             for (j = 0; j < n; ++j)
