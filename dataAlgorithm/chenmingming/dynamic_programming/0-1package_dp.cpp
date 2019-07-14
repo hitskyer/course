8ab1c1e7e2401a3f351889f9c20a11d2a12ac4ec
kobe_24o <mingmingchen1989@163.com>
2019-07-15 00:15:35 +0800

    Update 0-1package_dp.cpp

--- dataAlgorithm/chenmingming/dynamic_programming/0-1package_dp.cpp
+++ dataAlgorithm/chenmingming/dynamic_programming/0-1package_dp.cpp
@@ -1,37 +1,39 @@
 /**
- * @description: 0-1背包--dp应用引入案例
+ * @description: 0-1背包--dp应用
  * @author: michael ming
  * @date: 2019/7/9 1:13
  * @modified by: 
  */
 #include <iostream>
 #define MaxWeight 9   //背包承载极限
+const int N = 5;    //背包个数
 using namespace std;
-bool mem [5][9];
-int counttimes;
-void fill(int i, int curWeight, int *bag, int N, int &maxweightinbag)
+bool states[N][MaxWeight+1];//全局参数自动初始化，默认是false
+int fill(int i, int curWeight, int *bag, int N, int &maxweightinbag)
 {
-    cout << "调用次数: " << ++counttimes << endl;
-    if(curWeight == MaxWeight || i == N)//到达极限了，或者考察完所有物品了
+    states[0][0] = true;
+    if(bag[0] <= MaxWeight)
+        states[0][bag[0]] = true;
+    for(int i = 1; i < N; ++i)
     {
-        if(curWeight > maxweightinbag)
-            maxweightinbag = curWeight;//记录历史最大装载量
-        return;
+        for(int j = 0; j <= MaxWeight; ++j)
+        {
+            if(states[i-1][j] == true)
+                states[i][j] = states[i-1][j];
+        }
+        for(int j = 0; j <= MaxWeight-bag[i]; ++j)
+            if(states[i-1][j] == true)
+                states[i][j+bag[i]] = true;
     }
-    //-----注释掉以下3行查看效果-------
-    if(mem[i][curWeight])
-        return;
-    mem[i][curWeight] = true;
-    //---------------------------------
-    fill(i+1,curWeight,bag,N,maxweightinbag);//不选择当前i物品，cw不更新
-    if(curWeight+bag[i] <= MaxWeight)//选择当前i物品，cw更新
-    {//没有达到极限，继续装
-        fill(i+1,curWeight+bag[i],bag,N,maxweightinbag);
+    for(int i = MaxWeight; i >= 0; --i)
+    {
+        if(states[N-1][i] == true)
+            return i;
     }
+    return 0;
 }
 int main()
 {
-    const int N = 5;
     int bag[N] = {2,2,4,6,3};
     int maxweightinbag = 0;
     fill(0,0,bag,N,maxweightinbag);
