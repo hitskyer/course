4897a4a349a64b2125d3475c2a078a680501b8d0
kobe_24o <mingmingchen1989@163.com>
2019-07-20 00:49:29 +0800

    Create dp_lessen2_example.cpp

---
+++ dataAlgorithm/chenmingming/dynamic_programming/dp_lessen2_example.cpp
@@ -0,0 +1,38 @@
+/**
+ * @description: 
+ * @author: michael ming
+ * @date: 2019/7/19 23:30
+ * @modified by: 
+ */
+#include <iostream>
+#define N 4//地图大小
+using namespace std;
+int step_dp(int (*map)[N])
+{
+    int (*states)[N] = new int [N][N];
+    int i, j, sum = 0;
+    for(j = 0; j < N; ++j)//初始化第一行状态
+    {
+        sum += map[0][j];
+        states[0][j] = sum;
+    }
+    sum = 0;
+    for(i = 1; i < N; ++i)//初始化第一列状态
+    {
+        sum += map[i][0];
+        states[i][0] = sum;
+    }
+    for(i = 1; i < N; ++i)
+        for(j = 1; j < N; ++j)
+            states[i][j] = map[i][j]+min(states[i][j-1],states[i-1][j]);
+    return states[N-1][N-1];
+}
+int main()
+{
+    int map[N][N] = {1,3,5,9,2,1,3,4,5,2,6,7,6,8,4,3};
+    cout << "最短路径是：" << step_dp(map) << endl;
+    cout << "走过的点的距离分别是：" << endl;
+//    for(int i = 0; i < k; ++i)
+//        cout << shortestWay[i] << " ";
+    return 0;
+}
