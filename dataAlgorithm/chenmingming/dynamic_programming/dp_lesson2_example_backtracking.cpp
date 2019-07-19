9d3f27c271197f7122fe1c8f9f3a3918036ac467
kobe_24o <mingmingchen1989@163.com>
2019-07-19 13:54:21 +0800

    Create dp_lesson2_example_backtracking.cpp

---
+++ dataAlgorithm/chenmingming/dynamic_programming/dp_lesson2_example_backtracking.cpp
@@ -0,0 +1,15 @@
+/**
+ * @description: dp课第二节，案例回溯法求解
+ * @author: michael ming
+ * @date: 2019/7/19 12:55
+ * @modified by: 
+ */
+#include <iostream>
+#define N 4
+using namespace std;
+
+int main()
+{
+    int map[N][N] = {0,3,5,9,2,1,3,4,5,2,6,7,6,8,4,3};
+
+}
