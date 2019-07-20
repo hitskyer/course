88704d61812180f89f68c45b11e54289535715fb
kobe_24o <mingmingchen1989@163.com>
2019-07-21 01:23:44 +0800

    Update exchange_money_dp.cpp

--- dataAlgorithm/chenmingming/dynamic_programming/exchange_money_dp.cpp
+++ dataAlgorithm/chenmingming/dynamic_programming/exchange_money_dp.cpp
@@ -5,44 +5,35 @@
  * @modified by: 
  */
 #include <iostream>
+#include <algorithm>
+#include <memory.h>
+
 #define N 3
+const int targetMoney = 18;//目标金额
 int rmb[N] = {1,9,10};//钞票面额
-int amount[N];
-int minAmount[N];
+int mem[targetMoney+1];//备忘录，存放最小张数
 using namespace std;
-void exchange(const int &targetMoney, int curMoney, int &minPiece, int piece)
+int minP(int Money)
 {
-    if(curMoney > targetMoney)//超过目标，返回
-        return;
-    if(curMoney == targetMoney)//达到目标金额
-    {
-        if(piece < minPiece)
-        {
-            minPiece = piece;//更新最小张数
-            for(int i = 0; i < N; ++i)
-                minAmount[i] = amount[i];//获取每张钞票的张数
-        }
-        return;
-    }
+    if(Money < 0)//超过目标，返回很大的张数，表示不可能凑成
+        return 65535;
+    if(Money == 0)//达到目标金额
+        return 0;
+    if(mem[Money] > 0)//计算过了，直接读取备忘录
+        return mem[Money];
+    int minAmount[N];
+    memset(minAmount,65535,N*sizeof(int));
     for(int i = 0; i < N; ++i)
     {//递归调用,拿取每张面额的钞票
-        amount[i]++;
-        exchange(targetMoney,curMoney+rmb[i],minPiece,piece+1);
-        amount[i]--;//恢复上次的状态
+        minAmount[i] = minP(Money-rmb[i]);
     }
+    sort(minAmount,minAmount+N);
+    mem[Money] = minAmount[0]+1;//记录最小的张数
+    return mem[Money];
 }
 int main()
 {
-    int minPiece = 65535, piece = 0,targetMoney = 18, curMoney = 0;
-    exchange(targetMoney,curMoney,minPiece,piece);
-    cout << "凑成" << targetMoney << "元，最少需要：" << minPiece << "张(枚)。" << endl;
-    int i = 0;
-    while(i < N)
-    {
-        if(minAmount[i] != 0)
-            cout << minAmount[i] << "个" << rmb[i] << " ";
-        i++;
-    }
-    cout << endl;
-    cout << "----------------------" << endl;
+    cout << "凑成" << targetMoney << "元，最少需要："
+        << minP(targetMoney) << "张(枚)。" << endl;
+    //如何打印出选取钞票的面额和张数？？？
 }
