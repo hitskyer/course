e5417fa157785ad7da1e6bc465f6c16142f8a8f7
kobe_24o <mingmingchen1989@163.com>
2019-07-26 00:43:13 +0800

    Update longest_increasing_subsequence.cpp

--- dataAlgorithm/chenmingming/dynamic_programming/longest_increasing_subsequence.cpp
+++ dataAlgorithm/chenmingming/dynamic_programming/longest_increasing_subsequence.cpp
@@ -15,13 +15,27 @@ void lis(int *a, int N, int idx, int curLen, int &maxLen)
     for(int i = idx+1; i < N; ++i)
     {
         if(a[i-1] <= a[i])
-//            lis(a,N,i,curLen+1,maxLen);//回溯
-            curLen++;//只取上一行或者只留本行
+            lis(a,N,i,curLen+1,maxLen);//回溯
+//            curLen++;//只取上一行或者只留本行
         else
             lis(a,N,i,1,maxLen);
     }
     if(curLen > maxLen)
         maxLen = curLen;
+}
+//DP解法
+int len_dp(int *a, int N)
+{
+    int maxlen[N];
+    maxlen[0] = 1;
+    for(int i = 1; i < N; ++i)
+    {
+        if(a[i-1] <= a[i])
+            maxlen[i] = maxlen[i-1]+1;
+        else
+            maxlen[i] = maxlen[i-1];
+    }
+    return maxlen[N-1];
 }
 int main()
 {
@@ -30,4 +44,5 @@ int main()
     int arr[N] = {2,9,3,6,5,1,7};
     lis(arr,N,0,1,maxLen);
     cout << "arr数组最长递增子序列长度（回溯）：" << maxLen << endl;
+    cout << "arr数组最长递增子序列长度（DP）：" << len_dp(arr,N) << endl;
 }
