3f46aa3780e4a55bb0cec339bf557a16f4e0f2ad
kobe_24o <mingmingchen1989@163.com>
2019-07-29 00:46:39 +0800

    Update topological_sorting.cpp

--- dataAlgorithm/chenmingming/graph/topological_sorting.cpp
+++ dataAlgorithm/chenmingming/graph/topological_sorting.cpp
@@ -4,4 +4,20 @@
  * @date: 2019/7/29 0:36
  * @modified by: 
  */
-
+#include <list>
+using namespace std;
+class Graph
+{
+    int v;//顶点个数
+    list<int> *adj;//邻接表
+public:
+    Graph(int vn)
+    {
+        v = vn;
+        adj = new list<int> [v];
+    }
+    ~Graph()
+    {
+        delete [] adj;
+    }
+};
