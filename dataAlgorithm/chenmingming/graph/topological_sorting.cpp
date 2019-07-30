d73513a92d4d3ae9f7c040ba9e14fae37a88f17e
kobe_24o <mingmingchen1989@163.com>
2019-07-30 14:03:17 +0800

    Update topological_sorting.cpp

--- dataAlgorithm/chenmingming/graph/topological_sorting.cpp
+++ dataAlgorithm/chenmingming/graph/topological_sorting.cpp
@@ -5,34 +5,51 @@
  * @modified by: 
  */
 #include <list>
+#include <iostream>
+
 using namespace std;
+class G_Node
+{
+public:
+    char info;
+    int indegree;
+    G_Node(char ch = '/'):info(ch),indegree(0){};
+};
 class Graph
 {
     int v;//顶点个数
-    list<int> *adj;//邻接表
+    list<G_Node> *adj;//邻接表
 public:
     Graph(int vn)
     {
         v = vn;
-        adj = new list<int> [v];
+        adj = new list<G_Node> [v];
+        auto *pGNode = new G_Node [v];
+        cout << "请顺序输入节点的信息：" << endl;
+        char ch;
+        for(int i = 0; i < v; ++i)
+        {
+            cin >> pGNode[i].info;
+        }
     }
     ~Graph()
     {
         delete [] adj;
     }
-    void addEdge(int s, int t)//s先于t,边s->t
+    void addEdge(char s, char t)//s先于t,边s->t
     {
         adj[s].push_back(t);
     }
     void topoSortByKahn()
     {
         int *indegree = new int[v];//统计每个顶点的入度
-        int i, j, w;
+        int i, w;
+        list<int>::iterator it;
         for(i = 0; i < v; ++i)
         {
-            for(j = 0; j < adj[i].size(); ++j)
+            for(it = adj[i].begin(); it != adj[i].end(); ++it)
             {
-                w = adj[i].f
+                indegree[*]
             }
         }
     }
