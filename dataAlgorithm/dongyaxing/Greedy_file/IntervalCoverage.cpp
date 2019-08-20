5ec9cfd2cbea0543902bd8087ff3296a91fdeceb
RF-DYX <379992467@qq.com>
2019-08-20 13:26:26 +0800

    输出所有不连续的区间

--- dataAlgorithm/dongyaxing/Greedy_file/IntervalCoverage.cpp
+++ dataAlgorithm/dongyaxing/Greedy_file/IntervalCoverage.cpp
@@ -19,7 +19,7 @@ void sortSmallBig(int *arrmin, int *arrmax, int len)
 		flag = true;
 		for(int j = 0; j < len - i - 1; ++j)
 		{
-			if(arrmin[j] > arrmax[j + 1])
+			if(arrmin[j] > arrmin[j + 1])
 			{
 				swap(arrmin[j], arrmin[j + 1]);
 				swap(arrmax[j], arrmax[j + 1]);
@@ -31,12 +31,11 @@ void sortSmallBig(int *arrmin, int *arrmax, int len)
 	}
 }
 
-void Coverage(int *arrmin, int *arrmax, int len, char *c)
+void Coverage(int *arrmin, int *arrmax, int len, char *c, int k)
 {
-	sortSmallBig(arrmin, arrmax, len);
-	c[0] = '1';
-	int index = 0;
-	for(int i = 1; i < len; ++i)
+	c[k] = '1';
+	int index = k;
+	for(int i = k; i < len; ++i)
 	{
 		if(arrmin[i] >= arrmax[index])
 		{
@@ -57,12 +56,35 @@ void print(int *arrmin, int *arrmax, char *c, int len)
 	}
 }
 
+void printArr(int *arr, int len)
+{
+	for(int i = 0; i < len; ++i)
+	{
+		cout << arr[i] << ", ";
+	}
+	cout << endl;
+}
+
 int main()
 {
 	int Imin[6] = {0,2,3,1,5,8};
 	int Imax[6] = {3,4,5,5,9,10};
 	char c[6] = {'0','0','0','0','0','0'};
-	Coverage(Imin, Imax, 6, c);
-	print(Imin, Imax, c, 6);
+	sortSmallBig(Imin, Imax, 6);
+	printArr(Imin, 6);
+	printArr(Imax, 6);
+	
+	sortSmallBig(Imin, Imax, 6);
+	for(int k = 0; k < 5; ++k)
+	{
+		Coverage(Imin, Imax, 6, c, k);
+		print(Imin, Imax, c, 6);
+		cout << " *****8<*****" << endl;
+		for(int i = 0; i < 6; ++i)
+		{
+			c[i] = '0';
+		}
+	}
+	
 	return 0;
 }
