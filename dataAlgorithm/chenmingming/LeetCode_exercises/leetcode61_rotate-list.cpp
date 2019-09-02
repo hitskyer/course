258ed537cea41a4d8d86021aaa1b420a32fcaf40
kobe_24o <mingmingchen1989@163.com>
2019-09-03 00:49:29 +0800

    lc

---
+++ dataAlgorithm/chenmingming/LeetCode_exercises/leetcode61_rotate-list.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(k == 0 || !head ||!head->next)
+            return head;
+        int len = 0;
+        ListNode *tmp = head;
+        while(tmp)
+        {
+            ++len;
+            tmp = tmp->next;
+        }
+        k = k%len;
+        if(k == 0)
+            return head;
+        tmp = head;
+        int count = len-k-1;
+        while(count--)
+        {
+            tmp = tmp->next;
+        }
+        ListNode *newHead = tmp->next;
+        tmp->next = NULL;
+        tmp = newHead;
+        while(tmp->next)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next = head;
+        return newHead;
+    }
+};
