5d4e8af7635d7f591b80b3f3b6333f4bcaee64c4
kobe24o <mingmingchen1989@163.com>
2019-12-04 00:37:37 +0800

    Update corpusSplit.py

--- dnn/chenmingming/PosTagging/corpusSplit.py
+++ dnn/chenmingming/PosTagging/corpusSplit.py
@@ -1,5 +1,5 @@
 def corpusSplit(infile, sentenceList):  # 将语料分割为句子
-    fdi = open(infile, 'r', encoding='UTF-8')  # 打开原始数据
+    fdi = open(infile, 'r', encoding='utf-8')  # 打开原始数据
     fullStopDict = {"。": 1, "；": 1, "？": 1, "！": 1}
     for line in fdi:
         text = line.strip()  # 删除左右空格
@@ -36,10 +36,10 @@ def corpusSplit(infile, sentenceList):  # 将语料分割为句子
 def out(sentenceList, out_dir):
     fdo_train_list = []
     for i in range(18):
-        fdo_train = open(out_dir + "/train.%d" % (i), "w")
+        fdo_train = open(out_dir + "/train.%d" % (i), "w", encoding='utf-8')
         fdo_train_list.append(fdo_train)
-    fdo_dev = open(out_dir + "/dev.txt", "w")
-    fdo_test = open(out_dir + "/test.txt", "w")
+    fdo_dev = open(out_dir + "/dev.txt", "w", encoding='utf-8')
+    fdo_test = open(out_dir + "/test.txt", "w", encoding='utf-8')
     for sindx in range(len(sentenceList)):
         if sindx % 20 < 18:
             for i in range(sindx % 20, 18):
@@ -57,14 +57,17 @@ def out(sentenceList, out_dir):
 import sys
 import re
 import random
-
+'''
 try:
     infile = sys.argv[1]
     out_dir = sys.argv[2]
 except:
     sys.stderr.write("\tpython " + sys.argv[0] + " infile out_dir\n")
     sys.exit(-1)
+'''
 # step 1 : 将语料分割为句子
+infile = ".\data\corpus4pos_tagging.txt"
+out_dir = ".\splitData"
 sentenceList = []
 corpusSplit(infile, sentenceList)
 # step 2 : 输出
