369ffc59d6d9b8cc36d3165a72cc59b11ebc1a7f
kobe_24o <mingmingchen1989@163.com>
2019-12-04 19:42:47 +0800

    Update corpusSplit.py

--- dnn/chenmingming/PosTagging/corpusSplit.py
+++ dnn/chenmingming/PosTagging/corpusSplit.py
@@ -6,24 +6,24 @@ def corpusSplit(infile, sentenceList):  # 将语料分割为句子
         if text == "":
             continue
         else:
-            infs = text.split()
+            infs = text.split() # 将所有单词分开
             sentence = []
             flag = True
             for s in infs:
-                w_p = s.split("/")
+                w_p = s.split("/")  # 返回分割后的字符串列表
                 if len(w_p) == 2:
                     word = w_p[0]
-                    if word.startswith("[") == 0:
-                        word = word.replace("[", "")
+                    if word.startswith("["):
+                        word = word.replace("[", "")  # 以[开始的，删除[
                     pos = w_p[1]
-                    pos = re.sub("].*", "", pos)
+                    pos = re.sub("].*", "", pos)  # re正则表达式模块替换掉后面的]
                     if word == "" or pos == "":
                         flag = False
                     else:
                         sentence.append(word + "/" + pos)
                     if word in fullStopDict:
                         if flag == True:
-                            sentenceList.append(" ".join(sentence))
+                            sentenceList.append(" ".join(sentence)) # 序列中元素用空格隔开
                         flag = True
                         sentence = []
                 else:
@@ -33,7 +33,7 @@ def corpusSplit(infile, sentenceList):  # 将语料分割为句子
     fdi.close()
 
 
-def out(sentenceList, out_dir):
+def out(sentenceList, out_dir): # 将句子分别写到20个文件中，18个训练文件
     fdo_train_list = []
     for i in range(18):
         fdo_train = open(out_dir + "/train.%d" % (i), "w", encoding='utf-8')
@@ -45,11 +45,11 @@ def out(sentenceList, out_dir):
             for i in range(sindx % 20, 18):
                 fdo_train_list[i].write(sentenceList[sindx] + "\n")
         elif sindx % 20 == 18:
-            fdo_dev.write(sentenceList[sindx] + "\n")
+            fdo_dev.write(sentenceList[sindx] + "\n")   # 1个开发集
         elif sindx % 20 == 19:
-            fdo_test.write(sentenceList[sindx] + "\n")
+            fdo_test.write(sentenceList[sindx] + "\n")  # 1个测试集
     for i in range(18):
-        fdo_train_list[i].close()
+        fdo_train_list[i].close()   # 文件有开，有关
     fdo_dev.close()
     fdo_test.close()
 
@@ -66,8 +66,8 @@ except:
     sys.exit(-1)
 '''
 # step 1 : 将语料分割为句子
-infile = ".\data\corpus4pos_tagging.txt"
-out_dir = ".\splitData"
+infile = "./data/corpus4pos_tagging.txt"
+out_dir = "./splitData"
 sentenceList = []
 corpusSplit(infile, sentenceList)
 # step 2 : 输出
