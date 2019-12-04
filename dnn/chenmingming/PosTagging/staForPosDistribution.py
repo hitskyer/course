e0287908c2da588af2e86a82a640b48cbf947c79
kobe_24o <mingmingchen1989@163.com>
2019-12-04 20:40:13 +0800

    Update staForPosDistribution.py

--- dnn/chenmingming/PosTagging/staForPosDistribution.py
+++ dnn/chenmingming/PosTagging/staForPosDistribution.py
@@ -8,22 +8,22 @@ def sta(infile, pDict):
 	fdi = open(infile)
 	for line in fdi:
 		infs = line.strip().split()
-		posList = [s.split("/")[1] for s in infs]
+		posList = [s.split("/")[1] for s in infs]	# 词性list
 		for pos in posList:
-			add2posDict(pos, pDict)
-			add2posDict("all", pDict)
+			add2posDict(pos, pDict)	# 统计各个词性的次数
+			add2posDict("all", pDict)	# 总的次数
 	fdi.close()
 def out(pDict):
 	oList = list(pDict.items())
-	oList.sort(key=lambda infs:(infs[1]), reverse=True)
+	oList.sort(key=lambda infs:(infs[1]), reverse=True)	# 按匿名函数排序
 	total = oList[0][1]
 	for pos, num in oList:
-		print("%s\t%.4f" % (pos, num/total))
+		print("%s\t%.4f" % (pos, num/total))	# 打印 词性，对应频率
 try:
 	infile = sys.argv[1]
 except:
 	sys.stderr.write("\tpython "+sys.argv[0]+" infile\n")
 	sys.exit(-1)
 pDict = {}
-sta(infile, pDict)
+sta(infile, pDict)	# 统计训练集中的语料出现频率
 out(pDict)
