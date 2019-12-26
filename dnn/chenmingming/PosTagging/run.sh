0ef4af0435332ce5d189d3046ee0082f9df43c5b
kobe_24o <mingmingchen1989@163.com>
2019-12-26 22:40:18 +0800

    Update run.sh

--- dnn/chenmingming/PosTagging/run.sh
+++ dnn/chenmingming/PosTagging/run.sh
@@ -5,16 +5,16 @@ do
 	alias python='/usr/local/bin/python3.7'
 	# step 1 : 最大概率模型
 	# step 1.1 : 训练模型
-	python trainByMaxProb.py ./data/train.${i} ./data/model.MaxProb.${i}
+	# python trainByMaxProb.py ./data/train.${i} ./data/model.MaxProb.${i}
 	# step 1.2 : 在训练集上做评估
-	python predictByMaxProb.py ./data/train.${i} ./data/model.MaxProb.${i} ./data/train.${i}.MaxProb.predict
-	python resultEval.py ./data/train.${i} ./data/train.${i}.MaxProb.predict ./data/train.${i}.MaxProb.eval
+	# python predictByMaxProb.py ./data/train.${i} ./data/model.MaxProb.${i} ./data/train.${i}.MaxProb.predict
+	# python resultEval.py ./data/train.${i} ./data/train.${i}.MaxProb.predict ./data/train.${i}.MaxProb.eval
 	# step 1.3 : 在开发集上做评估
-	python predictByMaxProb.py ./data/dev.txt ./data/model.MaxProb.${i} ./data/dev.${i}.MaxProb.predict
-	python resultEval.py ./data/dev.txt ./data/dev.${i}.MaxProb.predict ./data/dev.${i}.MaxProb.eval
+	# python predictByMaxProb.py ./data/dev.txt ./data/model.MaxProb.${i} ./data/dev.${i}.MaxProb.predict
+	# python resultEval.py ./data/dev.txt ./data/dev.${i}.MaxProb.predict ./data/dev.${i}.MaxProb.eval
 	# step 1.4 : 在测试集上做评估
-	python predictByMaxProb.py ./data/test.txt ./data/model.MaxProb.${i} ./data/test.${i}.MaxProb.predict
-	python resultEval.py ./data/test.txt ./data/test.${i}.MaxProb.predict ./data/test.${i}.MaxProb.eval
+	# python predictByMaxProb.py ./data/test.txt ./data/model.MaxProb.${i} ./data/test.${i}.MaxProb.predict
+	# python resultEval.py ./data/test.txt ./data/test.${i}.MaxProb.predict ./data/test.${i}.MaxProb.eval
 	
 	# step 2 : BiHMM模型
 	# step 2.1 : 训练模型
@@ -28,5 +28,6 @@ do
 	# step 2.4 : 在测试集上做评估
 	python predictByBiHMM.py ./data/test.txt ./data/model.BiHMM.${i} ./data/test.${i}.BiHMM.predict
 	python resultEval.py ./data/test.txt ./data/test.${i}.BiHMM.predict ./data/test.${i}.BiHMM.eval
+	echo "BiHMM跑完 %i% 次"
 done
 echo "FINISH !!!"
