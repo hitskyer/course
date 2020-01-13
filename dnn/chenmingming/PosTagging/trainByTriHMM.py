29b2b8af3b3472f89adb0cfdefabed56a9b59b33
kobe_24o <mingmingchen1989@163.com>
2020-01-13 23:25:31 +0800

    Create trainByTriHMM.py

---
+++ dnn/chenmingming/PosTagging/trainByTriHMM.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+# Python 3.7
+# @Time: 2020/1/13 21:05
+# @Author: Michael Ming
+# @Website: https://michael.blog.csdn.net/
+# @File: trainByTriHMM.py
+
+import sys
+import math
+def add2transDict(pp_pos, p_pos, cur_pos, transDict):
+    if pp_pos in transDict:
+        if p_pos in transDict[pp_pos]:
+            if cur_pos in transDict[pp_pos][p_pos]:
+                transDict[pp_pos][p_pos][cur_pos] += 1
+            else:
+                transDict[pp_pos][p_pos][cur_pos] = 1
+        else:
+            transDict[pp_pos][p_pos] = {cur_pos : 1}
+    else:
+        transDict[pp_pos] = {pp_pos : {cur_pos : 1}}
+def sta(infile, transDict, emitDict):
+    f = open(infile, 'r', encoding='utf-8')
+    for line in f:
+        infos = line.strip().split()
+    wpList = [["__NONE__", "__start__"]] + [s.split("/") for s in infos] + [["__NONE__", "__end__"]]
+    for i in range(1, len(wpList)):
+        pp_pos = p_pos = ""
+        if i == 1:
+            pp_pos = p_pos = wpList[i-1][1]
+        else:
+            pp_pos = wpList[i-2][1]
+            p_pos = wpList[i-1][1]
+        cur_pos = wpList[i][1]
+        word = wpList[i][0]
+        if word == "" or cur_pos == "" or p_pos == "" or pp_pos == "":
+            continue
+        add2transDict(pp_pos, p_pos, cur_pos, transDict)
+        add2emitDict(cur_pos, word, emitDict)
+    f.close()
+try:
+    infile = sys.argv[1]
+    model_file = sys.argv[2]
+except:
+    sys.stderr.write("\tpython " + sys.argv[0] + " infile model_file\n")
+    sys.exit(-1)
+transDict = {}
+emitDict = {}
+
+sta(infile, transDict, emitDict)
+
+if __name__ == '__main__':
+    pass
