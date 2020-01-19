108bbbdb9df01d614539f18fc5e4b43bbd6091c1
kobe_24o <mingmingchen1989@163.com>
2020-01-20 00:42:16 +0800

    result

--- dnn/chenmingming/PosTagging/resultView.py
+++ dnn/chenmingming/PosTagging/resultView.py
@@ -11,6 +11,9 @@ testEval_maxProb = []
 trainEval_BiHMM = []
 devEval_BiHMM = []
 testEval_BiHMM = []
+trainEval_TriHMM = []
+devEval_TriHMM = []
+testEval_TriHMM = []
 for i in range(18):
     filename1 = "./data/train." + str(i) + ".MaxProb.eval"
     filename2 = "./data/dev." + str(i) + ".MaxProb.eval"
@@ -18,6 +21,9 @@ for i in range(18):
     filename4 = "./data/train." + str(i) + ".BiHMM.eval"
     filename5 = "./data/dev." + str(i) + ".BiHMM.eval"
     filename6 = "./data/test." + str(i) + ".BiHMM.eval"
+    filename7 = "./data/train." + str(i) + ".TriHMM.eval"
+    filename8 = "./data/dev." + str(i) + ".TriHMM.eval"
+    filename9 = "./data/test." + str(i) + ".TriHMM.eval"
     with open(filename1, 'r', encoding='utf-8') as f1:
         trainEval_maxProb.append(float(f1.readline().split()[2]))
     with open(filename2, 'r', encoding='utf-8') as f2:
@@ -30,17 +36,25 @@ for i in range(18):
         devEval_BiHMM.append(float(f5.readline().split()[2]))
     with open(filename6, 'r', encoding='utf-8') as f6:
         testEval_BiHMM.append(float(f6.readline().split()[2]))
+    with open(filename7, 'r', encoding='utf-8') as f7:
+        trainEval_TriHMM.append(float(f7.readline().split()[2]))
+    with open(filename8, 'r', encoding='utf-8') as f8:
+        devEval_TriHMM.append(float(f8.readline().split()[2]))
+    with open(filename9, 'r', encoding='utf-8') as f9:
+        testEval_TriHMM.append(float(f9.readline().split()[2]))
 
 import matplotlib.pyplot as plt
 
 # plt.rcParams['font.family'] = 'sans-serif'	# 消除中文乱码
 plt.rcParams['font.sans-serif'] = 'SimHei'	# 消除中文乱码
-plt.title("最大概率模型 & BiHMM模型 在不同大小语料下的准确率对比")
+plt.title("最大概率模型 & BiHMM模型 & TriHMM模型 在不同大小语料下的准确率对比")
 plt.xlabel("语料")
 plt.ylabel("准确率")
 plt.plot(trainEval_maxProb, 'r-', devEval_maxProb, 'b-', testEval_maxProb, 'g-',
-	trainEval_BiHMM, 'r--', devEval_BiHMM, 'b--', testEval_BiHMM, 'g--')
-plt.legend(('train_maxProb', 'dev_maxProb', 'test_maxProb', 'train_BiHMM', 'dev_BiHMM', 'test_BiHMM'), 
-	loc='upper right')
+	trainEval_BiHMM, 'r--', devEval_BiHMM, 'b--', testEval_BiHMM, 'g--',
+	trainEval_TriHMM, 'r*', devEval_TriHMM, 'b*', testEval_TriHMM, 'g*',)
+plt.legend(('train_maxProb', 'dev_maxProb', 'test_maxProb', 
+	'train_BiHMM', 'dev_BiHMM', 'test_BiHMM',
+	'train_TriHMM', 'dev_TriHMM', 'test_TriHMM'), loc=0)
 plt.savefig('resultView.jpg')
 plt.show()
