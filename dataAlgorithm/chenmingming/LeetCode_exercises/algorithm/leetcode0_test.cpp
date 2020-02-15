2247db96139bba802ae9d81ab7286ed9a7fede3c
kobe_24o <mingmingchen1989@163.com>
2020-02-15 10:43:48 +0800

    lc

--- dataAlgorithm/chenmingming/LeetCode_exercises/algorithm/leetcode0_test.cpp
+++ dataAlgorithm/chenmingming/LeetCode_exercises/algorithm/leetcode0_test.cpp
@@ -6,67 +6,53 @@
  */
 #include <bits/stdc++.h>
 using namespace std;
-struct ListNode {
-int val;
- ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
-  };
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *emptyHead = new ListNode(-1);
-        emptyHead->next = head;
-        ListNode *prev = emptyHead, *start = emptyHead->next, *end = emptyHead;
-        ListNode *temp=NULL;
-        bool flag = false;
-        int count;
-        while(end)
+    bool oneEditAway(string a, string b) {
+        int m = a.size(), n = b.size(), i, j;
+        if(abs(m-n) >= 2)
+            return false;
+        if(m==0 || n==0)
+            return true;
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        //初始化第一行，第一列的编辑距离
+        for(j = 0; j < n; j++)
         {
-            count = k;
-            start = prev->next;
-            while(end && count)
+            if(a[0]==b[j])	dp[0][j] = j;
+            else if(j != 0) dp[0][j] = 1+dp[0][j-1];
+            else 			dp[0][j] = 1;
+        }
+        for(i = 0; i < m; i++)
+        {
+            if(a[i]==b[0])	dp[i][0] = i;
+            else if(i != 0) dp[i][0] = 1+dp[i-1][0];
+            else 			dp[i][0] = 1;
+        }
+        //填写状态表
+        for(i = 1; i < m; ++i)
+        {
+            for(j = 1; j < n; ++j)
             {
-                end = end->next;
-                count--;
-            }
-            if(!end)//
-                flag = true;//这一段不足k个，提前结束了,allEnd是结尾
-            else
-            {
-                temp = end->next;
-                end->next = NULL;
-            }
-            if(!flag)
-            {
-                prev->next = reverseList(start);
-                start->next = temp;
-                prev = start;
-                end = start;
+                if(a[i] == b[j])
+                    dp[i][j] =   min(dp[i-1][j-1],dp[i-1][j]+1,dp[i][j-1]+1);
+                else
+                    dp[i][j] = min(1+dp[i-1][j-1],dp[i-1][j]+1,dp[i][j-1]+1);
             }
         }
-        return emptyHead->next;
+        return (dp[m-1][n-1] < 2);
     }
 
-    ListNode* reverseList(ListNode *head)
+    int min(int x, int y, int z)
     {
-        ListNode *prev = NULL, *nt = head->next;
-        while(head && head->next)
-        {
-            head->next = prev;
-            prev = head;
-            head = nt;
-            if(nt)
-                nt = nt->next;
-        }
-        head->next = prev;
-        return head;
+        int m = INT_MAX;
+        if(x < m)	m = x;
+        if(y < m)	m = y;
+        if(z < m)	m = z;
+        return m;
     }
 };
 int main()
 {
-//    Solution s;
-    multimap<int,int> m;
-    m.insert(make_pair(0,1));
-//    m.insert(make_pair(0,1));
-    cout << m.size();
+    Solution s;
+    cout << s.oneEditAway("","");
 }
