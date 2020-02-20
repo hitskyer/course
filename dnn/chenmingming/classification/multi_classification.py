d4d66f2a89392777dcc0fddb85dab8b3a846c4e0
kobe_24o <mingmingchen1989@163.com>
2020-02-20 19:52:22 +0800

    Update multi_classification.py

--- dnn/chenmingming/classification/multi_classification.py
+++ dnn/chenmingming/classification/multi_classification.py
@@ -22,8 +22,8 @@ def show_data_set(X, y, data):
     plt.plot(X[y == 0, 0], X[y == 0, 1], 'rs', label=data.target_names[0])
     plt.plot(X[y == 1, 0], X[y == 1, 1], 'bx', label=data.target_names[1])
     plt.plot(X[y == 2, 0], X[y == 2, 1], 'go', label=data.target_names[2])
-    plt.xlabel(data.feature_names[0])
-    plt.ylabel(data.feature_names[1])
+    plt.xlabel(data.feature_names[2])
+    plt.ylabel(data.feature_names[3])
     plt.title("鸢尾花2维数据")
     plt.legend()
     plt.rcParams['font.sans-serif'] = 'SimHei'  # 消除中文乱码
@@ -34,8 +34,10 @@ def plot_data(X, y):
     plt.plot(X[y == 0, 0], X[y == 0, 1], 'rs', label='setosa')
     plt.plot(X[y == 1, 0], X[y == 1, 1], 'bx', label='versicolor')
     plt.plot(X[y == 2, 0], X[y == 2, 1], 'go', label='virginica')
-    plt.xlabel("sepal length (cm)")
-    plt.ylabel("sepal width (cm)")
+    # plt.xlabel("sepal length (cm)")
+    # plt.ylabel("sepal width (cm)")
+    plt.xlabel("petal length (cm)")
+    plt.ylabel("petal width (cm)")
     plt.title("预测分类边界")
     plt.legend()
     plt.rcParams['font.sans-serif'] = 'SimHei'  # 消除中文乱码
@@ -59,7 +61,8 @@ def test1(X_train, X_test, y_train, y_test, multi_class='ovr', solver='liblinear
     predict_test = log_reg.predict(X_test)
     sys.stdout.write("LR(multi_class = %s, solver = %s) Test  Accuracy : %.4g\n" % (
         multi_class, solver, metrics.accuracy_score(y_test, predict_test)))
-    plot_decision_boundary(4, 8.5, 1.5, 4.5, lambda x: log_reg.predict(x)) # 4个特征下注释掉
+    # plot_decision_boundary(4, 8.5, 1.5, 4.5, lambda x: log_reg.predict(x)) # 4个特征下注释掉，前两特征
+    plot_decision_boundary(0.5, 7.5, 0, 3, lambda x: log_reg.predict(x))  # 4个特征下注释掉，后两特征
     plot_data(X_train, y_train)
 
 
@@ -78,7 +81,8 @@ def test2(X_train, X_test, y_train, y_test):
     predict_test = ovr.predict(X_test)
     sys.stdout.write("LR(ovr) Test  Accuracy : %.4g\n" % (
         metrics.accuracy_score(y_test, predict_test)))
-    plot_decision_boundary(4, 8.5, 1.5, 4.5, lambda x: ovr.predict(x))# 4个特征下注释掉
+    # plot_decision_boundary(4, 8.5, 1.5, 4.5, lambda x: ovr.predict(x))# 4个特征下注释掉，前两特征
+    plot_decision_boundary(0.5, 7.5, 0, 3, lambda x: ovr.predict(x))  # 4个特征下注释掉，后两特征
     plot_data(X_train, y_train)
 
 
@@ -93,7 +97,8 @@ def test3(X_train, X_test, y_train, y_test):
     predict_test = ovo.predict(X_test)
     sys.stdout.write("LR(ovo) Test  Accuracy : %.4g\n" % (
         metrics.accuracy_score(y_test, predict_test)))
-    plot_decision_boundary(4, 8.5, 1.5, 4.5, lambda x: ovo.predict(x)) # 4个特征下注释掉
+    # plot_decision_boundary(4, 8.5, 1.5, 4.5, lambda x: ovo.predict(x)) # 4个特征下注释掉,前两特征
+    plot_decision_boundary(0.5, 7.5, 0, 3, lambda x: ovo.predict(x))  # 4个特征下注释掉,后两特征
     plot_data(X_train, y_train)
 
 
@@ -102,11 +107,12 @@ if __name__ == '__main__':
     # print(dir(iris))    # 查看data所具有的属性或方法
     # print(iris.data)    # 数据
     # print(iris.DESCR)   # 数据描述
-    X = iris.data[:, :2]  # 取前2列特征（平面只能展示2维）
+    # X = iris.data[:, :2]  # 取前2列特征sepal（平面只能展示2维）
+    X = iris.data[:, 2:4]   # petal两个特征
     # X = iris.data  # 全部4个特征
     y = iris.target  # 分类
     show_data_set(X, y, iris)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=520)  # 默认test比例0.25
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=777)  # 默认test比例0.25
     test1(X_train, X_test, y_train, y_test, multi_class='ovr', solver='liblinear')
     test2(X_train, X_test, y_train, y_test)
     test1(X_train, X_test, y_train, y_test, multi_class='multinomial', solver='newton-cg')
