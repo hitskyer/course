3c90d5e094aea2d7e2cbbcdd931fbcb7307a7e8c
kobe_24o <mingmingchen1989@163.com>
2020-02-21 20:50:10 +0800

    copy lr4Postagging

---
+++ dnn/chenmingming/PosTagging/lr4postagging/CorpusFormtor.py
@@ -0,0 +1,45 @@
+
+class CorpusFormtor:
+	def __init__(self, pos_file, fig_file, fnumber):
+		self.__posDict = {}
+		self.__loadPosDict(pos_file, self.__posDict)
+		self.__fe = FeaExtractor(fig_file=fig_file, fnumber=fnumber)
+	def format4label_file(self, label_file, format_file):
+		fdi = open(label_file)
+		fdo = open(format_file, "w")
+		feaKeyList = ["label"]
+		self.__fe.getFeaKeyList(feaKeyList)
+		fdo.write(",".join(feaKeyList)+"\n")
+		for line in fdi:
+			wps = [s.split("/") for s in line.strip().split()]
+			for indx in range(len(wps)):
+				word, pos = wps[indx]
+				if pos in self.__posDict:
+					pindx = self.__posDict[pos]
+					feaList = [str(pindx)]
+					self.__fe.getFeaList(wps, indx, feaList)
+					fdo.write(",".join(feaList)+"\n")
+		fdi.close()
+		fdo.close()
+	def __loadPosDict(self, pos_file, posDict):
+		fdi = open(pos_file)
+		for line in fdi:
+			pos, indx = line.strip().split()
+			indx = int(indx)
+			posDict[pos] = indx
+		fdi.close()
+import sys
+from pandas import DataFrame
+from FeaExtractor import *
+if __name__ == "__main__":
+	try:
+		corpus_file = sys.argv[1]
+		pos_file    = sys.argv[2]
+		fig_file    = sys.argv[3]
+		fnumber     = int(sys.argv[4])
+		outfile     = sys.argv[5]
+	except:
+		sys.stderr.write("\tpython "+sys.argv[0]+" corpus_file pos_file fig_file fnumber outfile\n")
+		sys.exit(-1)
+	cf = CorpusFormtor(pos_file, fig_file, fnumber)
+	cf.format4label_file(corpus_file, outfile)
---
+++ dnn/chenmingming/PosTagging/lr4postagging/FeaSelector.py
@@ -0,0 +1,99 @@
+
+def readSentences(train_file, sentences):
+	fdi = open(train_file)
+	for line in fdi:
+		wps = []
+		for s in line.strip().split():
+			word,pos = s.split("/")
+			wps.append([word, pos])
+		sentences.append(wps)
+	fdi.close()
+def getPosDict(sentences, posDict):
+	tmpDict = {}
+	for wps in sentences:
+		for word, pos in wps:
+			if pos not in tmpDict:
+				tmpDict[pos] = 1
+			else:
+				tmpDict[pos] += 1
+	tmpList = list(tmpDict.items())
+	tmpList.sort(key=lambda infs:infs[1], reverse=True)
+	for pos, _ in tmpList:
+		posDict[pos] = len(posDict)
+def sta4one(cfeaDict, total, indx, feaDict):
+	for fea in cfeaDict:
+		if fea in feaDict:
+			feaDict[fea][indx] += 1
+		else:
+			feaDict[fea] = [0 for i in range(total)]
+			feaDict[fea][indx] += 1
+	if "__all__" in feaDict:
+		feaDict["__all__"][indx] += 1
+	else:
+		feaDict["__all__"] = [0 for i in range(total)]
+		feaDict["__all__"][indx] += 1
+def sta(sentences, posDict, feaDict):
+	fe = FeaExtractor()
+	for wps in sentences:
+		for i in range(len(wps)):
+			cfeaDict = {}
+			fe.getFeaDict(wps, i, cfeaDict)
+			_, pos = wps[i]
+			sta4one(cfeaDict, len(posDict), posDict[pos], feaDict)
+def compute4entropy(lst):
+	total = sum(lst)
+	if total == 0:
+		return 0.0
+	entropy = 0.0
+	for f in lst:
+		if f > 0:
+			prob = f/total
+			entropy -= prob * math.log(prob)
+	return entropy
+def compute4one(feaDict, fea, entropy4all):
+	lst1 = feaDict["__all__"]
+	lst2 = feaDict[fea]
+	lst3 = [lst1[i]-lst2[i] for i in range(len(lst1))]
+	total = sum(lst1)
+	tprob = sum(lst2)/total
+	_tprob = sum(lst3)/total
+	return entropy4all - tprob * compute4entropy(lst2) - _tprob * compute4entropy(lst3)
+def compute(feaDict, igDict):
+	entropy4all = compute4entropy(feaDict["__all__"])
+	for fea in feaDict:
+		if sum(feaDict[fea]) > 0 and fea != "__all__":
+			igDict[fea] = compute4one(feaDict, fea, entropy4all)
+def out1(igDict, fig_file):
+	igList = list(igDict.items())
+	igList.sort(key=lambda infs:infs[1], reverse=True)
+	fdo = open(fig_file, "w")
+	for fea, ig in igList:
+		fdo.write("%s\t%f\n" % (fea, ig))
+	fdo.close()
+def out2(posDict, pos_file):
+	tmpList = list(posDict.items())
+	tmpList.sort(key=lambda infs:infs[1])
+	fdo = open(pos_file, "w")
+	for pos, indx in tmpList:
+		fdo.write("%s\t%d\n" % (pos, indx))
+	fdo.close()
+import sys
+from FeaExtractor import *
+import math
+try:
+	train_file = sys.argv[1]
+	fig_file   = sys.argv[2]
+	pos_file   = sys.argv[3]
+except:
+	sys.stderr.write("\tpython "+sys.argv[0]+" train_file fig_file pos_file\n")
+	sys.exit(-1)
+sentences = []
+readSentences(train_file, sentences)
+posDict   = {}
+getPosDict(sentences, posDict)
+feaDict   = {}
+sta(sentences, posDict, feaDict)
+igDict = {}
+compute(feaDict, igDict)
+out1(igDict, fig_file)
+out2(posDict, pos_file)
