457bdce2138ddc43186a223d46ad98137437a173
kobe_24o <mingmingchen1989@163.com>
2020-02-25 05:32:40 +0800

    Update run.sh

--- dnn/chenmingming/PosTagging/lr4postagging/run.sh
+++ dnn/chenmingming/PosTagging/lr4postagging/run.sh
@@ -8,9 +8,9 @@ python CorpusFormtor.py ../data/train.0 ./data/pos.0 ./data/fig.0 ${fnumber} ./d
 python CorpusFormtor.py ../data/dev.txt ./data/pos.0 ./data/fig.0 ${fnumber} ./data/dev.0.csv
 python CorpusFormtor.py ../data/test.txt ./data/pos.0 ./data/fig.0 ${fnumber} ./data/test.0.csv
 # step 3 : 只用部分训练语料（否则运行速度太慢）
-head -${snumber} ./data/train.0.csv > ./data/train.0.${snumber}.csv
-head -${snumber} ./data/dev.0.csv > ./data/dev.0.${snumber}.csv
-head -${snumber} ./data/test.0.csv > ./data/test.0.${snumber}.csv
+head -${snumber} ../data/train.0.csv > ./data/train.0.${snumber}.csv
+head -${snumber} ../data/dev.0.csv > ./data/dev.0.${snumber}.csv
+head -${snumber} ../data/test.0.csv > ./data/test.0.${snumber}.csv
 # step 4 : 训练
 python train.py ./data/train.0.${snumber}.csv ./data/test.0.${snumber}.csv ./data/fig.0 ${fnumber} ./data/model.0.${snumber}_${fnumber}.ovr
 # step 5 : 预测
