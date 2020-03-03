d692a0db6bcbbc2fcb4265b149e49d1b858233f7
kobe_24o <mingmingchen1989@163.com>
2020-03-03 16:35:32 +0800

    Update 3.KNearestNeighbors.py

--- dnn/chenmingming/lihang_book_code/3.KNearestNeighbors.py
+++ dnn/chenmingming/lihang_book_code/3.KNearestNeighbors.py
@@ -24,7 +24,7 @@ def L_p(xi, xj, p=2):
         return 0
 
 
-def KNearNeighbors():
+class KNearNeighbors():
     def __init__(self, X_train, y_train, neighbors=3, p=2):
         self.n = neighbors
         self.p = p
@@ -33,27 +33,31 @@ def KNearNeighbors():
 
     def predict(self, X):
         knn_list = []
+        # 先在训练集中取n个点出来，计算距离
         for i in range(self.n):
             dist = np.linalg.norm(X - self.X_train[i], ord=self.p)
             knn_list.append((dist, self.y_train[i]))
+        # 再在剩余的训练集中取出剩余的，计算距离，有距离更近的，替换knn_list里最大的
         for i in range(self.n, len(self.X_train)):
             max_index = knn_list.index(max(knn_list, key=lambda x: x[0]))
             dist = np.linalg.norm(X - self.X_train[i], ord=self.p)
             if knn_list[max_index][0] > dist:
                 knn_list[max_index] = (dist, self.y_train[i])
+        # 取出所有的n个最近邻点的标签
         knn = [k[-1] for k in knn_list]
         count_pairs = Counter(knn)
-        #         max_count = sorted(count_pairs, key=lambda x: x)[-1]
+
+        # 次数最多的标签，排序后最后一个 标签：出现次数
         max_count = sorted(count_pairs.items(), key=lambda x: x[1])[-1][0]
         return max_count
 
     def score(self, X_test, y_test):
         right_count = 0
-        n = 10
-        for X, y in zip(X_test, y_test):
+        for X, y in zip(X_test, y_test):  # zip 同时遍历多个对象
             label = self.predict(X)
-            if label == y:
+            if math.isclose(label, y, rel_tol=1e-5):  # 浮点型相等判断
                 right_count += 1
+        print("准确率：%.4f" % (right_count / len(X_test)))
         return right_count / len(X_test)
 
 
@@ -67,7 +71,7 @@ if __name__ == '__main__':
         for j in range(i + 1, len(X)):
             for p in range(1, 5):
                 print("x%d,x%d的L%d距离是：%.2f" % (i + 1, j + 1, p, L_p(X[i], X[j], p)))
-    # ---------计----------------
+    # ---------鸢尾花K近邻----------------
     iris = load_iris()
     df = pd.DataFrame(iris.data, columns=iris.feature_names)
     df['label'] = iris.target
@@ -75,14 +79,23 @@ if __name__ == '__main__':
     plt.scatter(df[50:100][iris.feature_names[0]], df[50:100][iris.feature_names[1]], label=iris.target_names[1])
     plt.xlabel(iris.feature_names[0])
     plt.ylabel(iris.feature_names[1])
-    plt.legend()
 
-    data = np.array(df.iloc[:100, [0, 1, -1]])
+    data = np.array(df.iloc[:100, [0, 1, -1]])  # 取前2种花，前两个特征
     X, y = data[:, :-1], data[:, -1]
+
+    # 切分数据集，留20%做测试数据
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf = KNearNeighbors(X_train, y_train)
+    # KNN算法，近邻选择20个，距离度量L2距离
+    clf = KNearNeighbors(X_train, y_train, 20, 2)
+    # 预测测试点，统计正确率
     clf.score(X_test, y_test)
-    test_point = [6.0, 3.0]
-    print("测试点的类别是：%d" % clf.predict(test_point))
-    plt.plot(test_point[0], test_point[1], 'bo', label='test_point')
+
+    # 随意给一个点，用KNN预测其分类
+    test_point = [4.75, 2.75]
+    test_point_flower = '测试点' + iris.target_names[int(clf.predict(test_point))]
+    print("测试点的类别是：%s" % test_point_flower)
+    plt.plot(test_point[0], test_point[1], 'bx', label=test_point_flower)
+    plt.rcParams['font.sans-serif'] = 'SimHei'  # 消除中文乱码
+    plt.rcParams['axes.unicode_minus'] = False  # 正常显示负号
+    plt.legend()
     plt.show()
