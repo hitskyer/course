6e21b99839d07fe27a1fc2e8d08bda37dc664586
kobe_24o <mingmingchen1989@163.com>
2020-03-31 00:49:38 +0800

    ML

--- dnn/chenmingming/lihang_book_code/8.AdaBoost.py
+++ dnn/chenmingming/lihang_book_code/8.AdaBoost.py
@@ -31,152 +31,104 @@ print(clf.score(X_test, y_test))
 
 # ------抄一遍AdaBoost--------
 class AdaBoost:
-    def __init__(self, n_estimators=50, learning_rate=1.0):
-        self.clf_num = n_estimators  # 基本分类器数量
-        self.learning_rate = learning_rate  # 学习率
+    def __init__(self, X, y, tol=0.05, max_iter=10):
+        self.X = X  # 训练数据 实例
+        self.y = y  # 训练数据 标签
+        self.tol = tol  # 训练中止条件 right_rate>self.tol
+        self.max_iter = max_iter  # 最大迭代次数
+        self.w = np.full((X.shape[0]), 1 / X.shape[0])  # 初始化样本权重w
+        self.alpha = []  # 弱分类器权重
+        self.G = []  # 弱分类器
+        self.min_v = min(X) - 0.5  # 分类阈值下届
+        self.max_v = max(X) + 0.5  # 分类阈值上届
 
-    def init_args(self, datasets, labels):
-        self.X = datasets
-        self.Y = labels
-        self.M, self.N = datasets.shape
+    def _class(self):
+        """以带权重的分类误差最小为目标，选择最佳分类阈值"""
+        e_min = np.inf  # e_min 分类误差
+        v_best = None  # v_best 最佳分类阈值
+        sign = None  # sign 小于分类阈值的样本属于的标签类别
+        for v in np.arange(self.min_v, self.max_v + 0.5, 1):
+            # 遍历可能v_best可取值，寻找最优解
+            e_1 = -(self.y[self.X < v] - 1) * self.w[self.X < v]  # 假设小于阈值 分类为1 获取分类误差*2
+            e_2 = (self.y[self.X > v] + 1) * self.w[self.X > v]  # 假设大于阈值 分类为-1 获取分类误差*2
+            e = (e_1.sum() + e_2.sum()) / 2  # 计算整个分类误差
+            if e < 0.5:  # 若分类误差小于0.5 说明X<v y->1
+                flag = 1
+            else:  # 若分类误差大于0.5 取其反向 说明X<v y->-1
+                e = 1 - e
+                flag = -1
+            if e < e_min:  # 保留最优解
+                e_min = e
+                sign = flag
+                v_best = v
+        return v_best, sign, e_min
 
-        self.clf_sets = []  # 弱分类器集合
+    def updata_w(self):
+        """更新样本权重w"""
+        v, sign = self.G[-1]  # 以下2行 根据上一轮的弱分类器更新样本权重
+        alpha = self.alpha[-1]
+        G = np.zeros(self.y.size, dtype=int)  # 以下三行重建弱分类器
+        G[self.X < v] = sign
+        G[self.X > v] = -sign
+        # G_1=np.full((np.where(self.X<v))[0].shape[0],sign)
+        # G_2=np.full((np.where(self.X>v))[0].shape[0],-sign)
+        # G=np.hstack([G_1,G_2])
+        P = self.w * np.exp(-alpha * self.y * G)  # 以下2行根据公式8.4 8.5 更新样本权重
+        self.w = P / P.sum()
 
-        # 初始化权重
-        self.weights = [1.0 / self.M] * self.M
-        # G(x)系数 alpha
-        self.alpha = []
+    def base_estimator(self, X, i):
+        """计算单个弱分类器预测输出"""
+        v, sign = self.G[i]
+        _G_1 = np.full((np.where(X < v))[0].shape[0], sign)
+        _G_2 = np.full((np.where(X > v))[0].shape[0], -sign)
+        _G = np.hstack([_G_1, _G_2])
+        return _G
 
-    def _G(self, features, labels, weights):
-        m = len(features)
-        error = float("inf")  # 无穷大
-        best_v = 0.0
+    def fit(self):
+        """对训练数据进行学习"""
+        G = 0
+        for i in range(self.max_iter):
+            class_v, sign, e = self._class()  # 获取当前迭代最佳分类阈值
+            alpha = 1 / 2 * np.log((1 - e) / e)  # 计算本轮弱分类器的系数
+            self.alpha.append(alpha)  # 保存弱分类器系数
+            self.G.append((class_v, sign))  # 保存弱分类器
+            _G = self.base_estimator(self.X, i)  # 以下4行计算当前总分类器（之前所有弱分类器加权和）分类效率
+            G += alpha * _G
+            y_predict = np.sign(G)
+            error_rate = np.sum(np.abs(y_predict - self.y)) / 2 / self.y.shape[0]
+            if error_rate < self.tol:  # 满足中止条件 则跳出循环
+                print("迭代次数:", i + 1)
+                break
+            else:
+                self.updata_w()  # 若不满足，更新权重，继续迭代
 
-        # 对单维feature
-        features_min = min(features)
-        features_max = max(features)
+    def predict(self, X):
+        """对新数据进行预测"""
+        G = 0
+        for i in range(len(self.alpha)):
+            # 遍历每一个弱分类器，进行加权
+            _G = self.base_estimator(X, i)
+            alpha = self.alpha[i]
+            G += alpha * _G
+        y_predict = np.sign(G)
+        return y_predict.astype(int)
 
-        n_step = (features_max - features_min + self.learning_rate) // self.learning_rate
-
-        direct, compare_array = None, None
-
-        for i in range(1, int(n_step)):
-            v = features_min + self.learning_rate * i
-            if v not in features:
-                # 误分类计算
-                compare_array_positive = np.array(
-                    [1 if features[k] > v else -1 for k in range(m)])
-                weight_error_positive = sum(
-                    [weights[k] for k in range(m) if compare_array_positive[k] != labels[k]]
-                )
-                compare_array_nagetive = np.array(
-                    [-1 if features[k] > v else 1 for k in range(m)]
-                )
-                weight_error_nagetive = sum([
-                    weights[k] for k in range(m) if compare_array_nagetive[k] != labels[k]
-                ])
-
-                if weight_error_positive < weight_error_nagetive:
-                    weight_error = weight_error_positive
-                    _compare_array = compare_array_positive
-                    direct = 'positive'
-                else:
-                    weight_error = weight_error_nagetive
-                    _compare_array = compare_array_nagetive
-                    direct = 'nagetive'
-                if weight_error < error:
-                    error = weight_error
-                    compare_array = _compare_array
-                    best_v = v
-        return best_v, direct, error, compare_array
-
-    def _alpha(self, error):
-        return 0.5 * np.log((1 - error) / error)
-
-    def _Z(self, weights, a, clf):  # 规范化因子
-        return sum([
-            weights[i] * np.exp(-1 * a * self.Y[i] * clf[i]) for i in range(self.M)
-        ])
-
-    def _w(self, a, clf, Z):  # 权值更新
-        for i in range(self.M):
-            self.weights[i] = self.weights[i] * np.exp(-1 * a * self.Y[i] * clf[i]) / Z
-
-    # G(x) 的线性组合
-    def _f(self, alpha, clf_sets):
-        pass
-
-    def G(self, x, v, direct):
-        if direct == 'positive':
-            return 1 if x > v else -1
-        else:
-            return -1 if x > v else 1
-
-    def fit(self, X, y):
-        axis = 0
-        final_dircet = None
-        self.init_args(X, y)
-        for epoch in range(self.clf_num):
-            best_clf_error, best_v, clf_result = 100000, None, None
-            # 根据特征维度，选择误差最小的
-            for j in range(self.N):
-                features = self.X[:, j]
-                # 分类阈值，分类误差，分类结果
-                v, direct, error, compare_array = self._G(
-                    features, self.Y, self.weights
-                )
-                if error < best_clf_error:
-                    best_clf_error = error
-                    best_v = v
-                    final_dircet = direct
-                    clf_result = compare_array
-                    axis = j
-
-                if best_clf_error == 0:
-                    break
-            # 计算 G(x) 系数 a
-            a = self._alpha(best_clf_error)
-            self.alpha.append(a)
-            # 记录分类器
-            self.clf_sets.append((axis, best_v, final_dircet))
-            # 规范化因子
-            Z = self._Z(self.weights, a, clf_result)
-            # 权值更想
-            self._w(a, clf_result, Z)
-
-    def predict(self, feature):
-        result = 0.0
-        for i in range(len(self.clf_sets)):
-            axis, clf_v, direct = self.clf_sets[i]
-            f_input = feature[axis]
-            result += self.alpha[i] * self.G(f_input, clf_v, direct)
-        # sign
-        return 1 if result > 0 else -1
-
-    def score(self, X_test, y_test):
-        right_count = 0
-        for i in range(len(X_test)):
-            feature = X_test[i]
-            if self.predict(feature) == y_test[i]:
-                right_count += 1
-        return right_count / len(X_test)
+    def score(self, X, y):
+        """对训练效果进行评价"""
+        y_predict = self.predict(X)
+        error_rate = np.sum(np.abs(y_predict - y)) / 2 / y.shape[0]
+        return 1 - error_rate
 
 
-def create_data():
-    iris = load_iris()
-    df = pd.DataFrame(iris.data, columns=iris.feature_names)
-    df['label'] = iris.target
-    df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'label']
-    data = np.array(df.iloc[:100, [0, 1, -1]])
-    for i in range(len(data)):
-        if data[i, -1] == 0:
-            data[i, -1] = -1
-    # print(data)
-    return data[:, :2], data[:, -1]
+def main():
+    X = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y = np.array([1, 1, 1, -1, -1, -1, 1, 1, 1, -1])
+    clf = AdaBoost(X, y)
+    clf.fit()
+    y_predict = clf.predict(X)
+    score = clf.score(X, y)
+    print("原始输出:", y)
+    print("预测输出:", y_predict)
+    print("预测正确率：{:.2%}".format(score))
 
-
-X, y = create_data()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-clf = AdaBoost(n_estimators=3, learning_rate=0.5)
-clf.fit(X_train, y_train)
-print(clf.score(X_test, y_test))
+main()
