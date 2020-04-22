2c319eda94fef7ea356bb0dd90986e3a2bb2327a
kobe_24o <mingmingchen1989@163.com>
2020-04-23 00:07:13 +0800

    Update 15.svd_pic_compress.py

--- dnn/chenmingming/lihang_book_code/15.svd_pic_compress.py
+++ dnn/chenmingming/lihang_book_code/15.svd_pic_compress.py
@@ -10,50 +10,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def zip_img_by_svd(img, rate=0.8):
-    plt.figure(figsize=(10, 10))
-    plt.rcParams['font.sans-serif'] = 'SimHei'  # 消除中文乱码
-    # plt.title()
-    f1 = plt.subplot(121)
-    f1.imshow(img)
-    f1.set_title("原始图片")
+def zip_img_by_svd(img, plotId, rate=0.8):
     zip_img = np.zeros(img.shape)
     u_shape = 0
     sigma_shape = 0
     vT_shape = 0
-    for chanel in range(3):
-        u, sigma, v = np.linalg.svd(img[:, :, chanel])
+
+    for chanel in range(3):  # 3个图层
+        u, sigma, v = np.linalg.svd(img[:, :, chanel])  # numpy svd函数
         sigma_i = 0
         temp = 0
 
-        while (temp / np.sum(sigma)) < rate:
+        while (temp / np.sum(sigma)) < rate:  # 选取的奇异值和需要达到设定的权重
             temp += sigma[sigma_i]
             sigma_i += 1
-        SigmaMat = np.zeros((sigma_i, sigma_i))
+        SigmaMat = np.zeros((sigma_i, sigma_i))  # 选取了sigma_i 最大的奇异值
         for i in range(sigma_i):
-            SigmaMat[i, i] = sigma[i]
+            SigmaMat[i, i] = sigma[i]  # 将奇异值填充到Sigma对角矩阵
         zip_img[:, :, chanel] = u[:, 0:sigma_i].dot(SigmaMat).dot(v[0:sigma_i, :])
+        # 将分解得到的3个矩阵相乘，得到压缩后的近似矩阵
         u_shape = u[:, 0:sigma_i].shape
         sigma_shape = SigmaMat.shape
         vT_shape = v[0:sigma_i, :].shape
 
-    for i in range(3):
+    for i in range(3):  # 对三个通道的矩阵数值进行归一化处理
         MAX = np.max(zip_img[:, :, i])
         MIN = np.min(zip_img[:, :, i])
         zip_img[:, :, i] = (zip_img[:, :, i] - MIN) / (MAX - MIN)
-    zip_img = np.round(zip_img * 255).astype("uint8")  # 不乘255是黑的，数据类型uint8
-    plt.imsave("zip_svd_img.jpg", zip_img)
-    # plt.figure(figsize=(10, 10))
-    # plt.imshow(zip_img)
-    # plt.title("压缩图片")
-    f2 = plt.subplot(122)
-    f2.imshow(zip_img)
-    f2.set_title("压缩图片")
-    plt.suptitle('对比')
-    plt.show()
 
+    zip_img = np.round(zip_img * 255).astype("uint8")  # 不乘255图片是黑的，数据类型uint8
+    plt.imsave("zip_svd_img.jpg", zip_img)  # 保存压缩后的图片
     zip_rate = (img.size - 3 * (
             u_shape[0] * u_shape[1] + sigma_shape[0] * sigma_shape[1] + vT_shape[0] * vT_shape[1])) / (zip_img.size)
+
+    f = plt.subplot(3, 3, plotId)
+    f.imshow(zip_img)
+    f.set_title("SVD压缩率 %.4f,奇异值数量：%d" % (zip_rate, sigma_i))
+
     print("设置的压缩率：", rate)
     print("使用的奇异值数量：", sigma_i)
     print("原始图片大小：", img.shape)
@@ -63,6 +56,14 @@ def zip_img_by_svd(img, rate=0.8):
 
 if __name__ == '__main__':
     imgfile = "svd_img.jpg"
+    plt.figure(figsize=(12, 12))
+    plt.rcParams['font.sans-serif'] = 'SimHei'  # 消除中文乱码
     img = plt.imread(imgfile)
-
-    zip_img_by_svd(img, rate=0.8)
+    f1 = plt.subplot(331)  # 绘制子图，3行3列，3*3个子图，现在画第1幅
+    f1.imshow(img)
+    f1.set_title("原始图片")
+    for i in range(8):  # 再画8个子图
+        rate = (i + 1) / 10.0  # 压缩率 10% - 80%
+        zip_img_by_svd(img, i + 2, rate)
+    plt.suptitle('图片SVD效果对比', fontsize=17, y=0.02)  # y偏移距离
+    plt.show()
