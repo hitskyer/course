0c6e0d49b6af5685decd413918664451769309b2
kobe_24o <mingmingchen1989@163.com>
2020-08-23 23:46:22 +0800

    ML

---
+++ python/chenmingming/fluent_python/chapter1/ex1.1.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# @Python Version: 3.7
+# @Time: 2020/8/22 10:03
+# @Author: Michael Ming
+# @Website: https://michael.blog.csdn.net/
+# @File: ex1.1.py
+# @Reference: 
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+print(Card)
