50900664f49c79bf8aa89b76f7dab8f3505a206f
kobe_24o <mingmingchen1989@163.com>
2021-01-27 22:44:39 +0800

    tf

--- dnn/chenmingming/tf2/tf_2_model_train.py
+++ dnn/chenmingming/tf2/tf_2_model_train.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 
 # tf.keras.Model  tf.keras.layers 构建模型
 # tf.keras.losses 损失函数
@@ -6,8 +6,10 @@
 # tf.keras.metrics 模型评估
 
 import tensorflow as tf
-X = tf.constant([[1.0, 2.0, 3.0],[4.0, 5.0, 6.0]])
-y = tf.constant([[10.0],[20.0]])
+
+X = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+y = tf.constant([[10.0], [20.0]])
+
 
 class Linear(tf.keras.Model):
     def __init__(self):
@@ -18,9 +20,11 @@ class Linear(tf.keras.Model):
             kernel_initializer=tf.zeros_initializer(),
             bias_initializer=tf.zeros_initializer()
         )
+
     def call(self, input):
         output = self.dense(input)
         return output
+
 
 model = Linear()
 optimizer = tf.keras.optimizers.SGD(learning_rate=0.001)
@@ -29,20 +33,22 @@ print(model.variables)
 for i in range(100):
     with tf.GradientTape() as tape:
         y_pred = model(X)
-        loss = tf.reduce_mean(tf.square(y_pred-y))
+        loss = tf.reduce_mean(tf.square(y_pred - y))
     grads = tape.gradient(loss, model.variables)
-    optimizer.apply_gradients(grads_and_vars=zip(grads,model.variables))
+    optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
 
 print(model.variables)
 
-#%%
+# %%
 # 手写数字 tf 实现
 import numpy as np
+
+
 class MNistLoader():
     def __init__(self):
         data = tf.keras.datasets.mnist
         # 加载数据
-        (self.train_data, self.train_label),(self.test_data, self.test_label) = data.load_data()
+        (self.train_data, self.train_label), (self.test_data, self.test_label) = data.load_data()
         # 扩展维度，灰度图1通道 [batch_size, 28, 28, chanels=1]
         self.train_data = np.expand_dims(self.train_data.astype(np.float32) / 255.0, axis=-1)
         self.test_data = np.expand_dims(self.test_data.astype(np.float32) / 255.0, axis=-1)
@@ -50,12 +56,14 @@ class MNistLoader():
         self.test_label = self.test_label.astype(np.int32)
         # 样本个数
         self.num_train_data, self.num_test_data = self.train_data.shape[0], self.test_data.shape[0]
+
     def get_batch(self, batch_size):
         # 从训练集里随机取出 batch_size 个样本
         idx = np.random.randint(0, self.num_train_data, batch_size)
         return self.train_data[idx, :], self.train_label[idx]
 
-#%%
+
+# %%
 
 # 自定义多层感知机模型
 class MLPmodel(tf.keras.Model):
@@ -65,6 +73,7 @@ class MLPmodel(tf.keras.Model):
         self.flatten = tf.keras.layers.Flatten()
         self.dense1 = tf.keras.layers.Dense(units=100, activation='relu')
         self.dense2 = tf.keras.layers.Dense(units=10)
+
     def call(self, input):
         x = self.flatten(input)
         x = self.dense1(x)
@@ -72,7 +81,8 @@ class MLPmodel(tf.keras.Model):
         output = tf.nn.softmax(x)
         return output
 
-#%%
+
+# %%
 
 # CNN 模型
 class myCNN(tf.keras.Model):
@@ -80,36 +90,37 @@ class myCNN(tf.keras.Model):
         super().__init__()
         self.conv1 = tf.keras.layers.Conv2D(
             filters=32,
-            kernel_size=[5,5],
+            kernel_size=[5, 5],
             padding='same',
             activation='relu'
         )
-        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=[2,2],strides=2)
+        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2)
         self.conv2 = tf.keras.layers.Conv2D(
             filters=64,
-            kernel_size=[5,5],
+            kernel_size=[5, 5],
             padding='same',
             activation='relu'
         )
-        self.pool2 = tf.keras.layers.MaxPool2D(pool_size=[2,2],strides=2)
-        self.flatten = tf.keras.layers.Reshape(target_shape=(7*7*64,))
-        self.dense1 = tf.keras.layers.Dense(units=1024,activation='relu')
+        self.pool2 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2)
+        self.flatten = tf.keras.layers.Reshape(target_shape=(7 * 7 * 64,))
+        self.dense1 = tf.keras.layers.Dense(units=1024, activation='relu')
         self.dense2 = tf.keras.layers.Dense(units=10)
-    def call(self, inputs): # [m, 28 , 28 , 1]
+
+    def call(self, inputs):  # [m, 28 , 28 , 1]
         # (n+2p-f)/s + 1，same 保持尺寸不变
-        x = self.conv1(inputs) # [m, 28 , 28 , 32]
+        x = self.conv1(inputs)  # [m, 28 , 28 , 32]
         # 池化时 p 常为 0
-        x = self.pool1(x) # [m, 14 , 14 , 32]
-        x = self.conv2(x) # [m, 14 , 14 , 64]
-        x = self.pool2(x) # [m, 7 , 7 , 64]
-        x = self.flatten(x) # [m, 7*7*64]
-        x = self.dense1(x) # [m, 1024]
-        x = self.dense2(x) # [m, 10]
-        outputs = tf.nn.softmax(x) # [m, 10]
+        x = self.pool1(x)  # [m, 14 , 14 , 32]
+        x = self.conv2(x)  # [m, 14 , 14 , 64]
+        x = self.pool2(x)  # [m, 7 , 7 , 64]
+        x = self.flatten(x)  # [m, 7*7*64]
+        x = self.dense1(x)  # [m, 1024]
+        x = self.dense2(x)  # [m, 10]
+        outputs = tf.nn.softmax(x)  # [m, 10]
         return outputs
 
 
-#%%
+# %%
 
 num_epochs = 5
 batch_size = 50
@@ -117,33 +128,55 @@ learning_rate = 1e-4
 
 # mymodel = MLPmodel()
 # mymodel = myCNN()
-mymodel = tf.keras.applications.MobileNetV2()
+# mymodel = tf.keras.applications.MobileNetV2()
 
+# mymodel = tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(100, activation='relu'),
+#     tf.keras.layers.Dense(10),
+#     tf.keras.layers.Softmax()
+# ])
+
+inp = tf.keras.Input(shape=(28, 28, 1))
+x = tf.keras.layers.Flatten()(inp)
+x = tf.keras.layers.Dense(units=100, activation=tf.nn.relu)(x)
+x = tf.keras.layers.Dense(units=10)(x)
+out = tf.keras.layers.Softmax()(x)
+mymodel = tf.keras.Model(inputs=inp, outputs=out)
+
+# %%
 data_loader = MNistLoader()
-optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+# optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+#
+# num_batches = int(data_loader.num_train_data // batch_size * num_epochs)
+# for idx in range(num_batches):
+#     X, y = data_loader.get_batch(batch_size)
+#     with tf.GradientTape() as tape:
+#         y_pred = mymodel(X)
+#         loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+#         loss = tf.reduce_mean(loss)
+#         print("batch {}, loss {}".format(idx, loss.numpy()))
+#     grads = tape.gradient(loss, mymodel.variables)
+#     optimizer.apply_gradients(grads_and_vars=zip(grads, mymodel.variables))
 
-num_batches = int(data_loader.num_train_data//batch_size * num_epochs)
-for idx in range(num_batches):
-    X,y = data_loader.get_batch(batch_size)
-    with tf.GradientTape() as tape:
-        y_pred = mymodel(X)
-        loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
-        loss = tf.reduce_mean(loss)
-        print("batch {}, loss {}".format(idx, loss.numpy()))
-    grads = tape.gradient(loss, mymodel.variables)
-    optimizer.apply_gradients(grads_and_vars=zip(grads, mymodel.variables))
+mymodel.compile(
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate),
+    loss=tf.keras.losses.sparse_categorical_crossentropy,
+    metrics=[tf.keras.metrics.sparse_categorical_accuracy]
+)
+mymodel.fit(data_loader.train_data, data_loader.train_label,
+            batch_size=batch_size,epochs=num_epochs)
+# %%
 
-#%%
+# sparse_categorical_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+# num_batches = int(data_loader.num_test_data // batch_size)
+# for idx in range(num_batches):
+#     start, end = idx * batch_size, (idx + 1) * batch_size
+#     y_pred = mymodel.predict(data_loader.test_data[start: end])
+#     sparse_categorical_accuracy.update_state(y_true=data_loader.test_label[start:end],
+#                                              y_pred=y_pred)
+# print("test 准确率：{}".format(sparse_categorical_accuracy.result()))
 
-sparse_categorical_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
-num_batches = int(data_loader.num_test_data // batch_size)
-for idx in range(num_batches):
-    start, end = idx*batch_size, (idx+1)*batch_size
-    y_pred = mymodel.predict(data_loader.test_data[start : end])
-    sparse_categorical_accuracy.update_state(y_true=data_loader.test_label[start:end],
-                                             y_pred=y_pred)
-print("test 准确率：{}".format(sparse_categorical_accuracy.result()))
-
-#%%
-
-
+res = mymodel.evaluate(data_loader.test_data, data_loader.test_label)
+print(res) # [loss, acc]
+# %%
---
+++ dnn/chenmingming/tf2/tf_5_mylayers_loss_metrics.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+
+
+class myLayer(tf.keras.layers.Layer):
+    def __init__(self, units):
+        super().__init__()
+        self.units = units
+
+    def build(self, input_shape):  # input_shape 是一个tensor
+        # input_shape 是第一次运行 call() 时参数inputs的形状
+        # 第一次使用该层的时候，调用build
+        self.w = self.add_weight(name='w',
+                                 shape=[input_shape[-1], self.units],
+                                 initializer=tf.zeros_initializer())
+        self.b = self.add_weight(name='b',
+                                 shape=[self.units],
+                                 initializer=tf.zeros_initializer())
+
+    def call(self, inputs):
+        y_pred = tf.matmul(inputs, self.w) + self.b
+        return y_pred
+
+
+class LinearModel(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.dense = myLayer(units=1)
+
+    def call(self, inputs):
+        output = self.dense(inputs)
+        return output
+
+
+import numpy as np
+
+# 原始数据
+X_raw = np.array([0.0, 1., 2., 3., 4.], dtype=np.float32)
+y_raw = np.array([0.01, 2., 4., 5.98, 8.], dtype=np.float32)
+
+X = np.expand_dims(X_raw, axis=-1)
+y = np.expand_dims(y_raw, axis=-1)
+
+# 转成张量
+X = tf.constant(X)
+y = tf.constant(y)
+
+
+class myError(tf.keras.losses.Loss):
+    def call(self, y_true, y_pred):
+        return tf.reduce_mean(tf.square(y_true - y_pred))
+
+
+class myMetric(tf.keras.metrics.Metric):
+    def __init__(self):
+        super().__init__()
+        self.total = self.add_weight(name='total',
+                                     dtype=tf.int32,
+                                     initializer=tf.zeros_initializer())
+        self.count = self.add_weight(name='count',
+                                     dtype=tf.int32,
+                                     initializer=tf.zeros_initializer())
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        values = tf.cast(tf.abs(y_true - y_pred) < 0.1, tf.int32)
+        self.total.assign_add(tf.shape(y_true)[0])
+        self.count.assign_add(tf.reduce_sum(values))
+
+    def result(self):
+        return self.count / self.total
+
+
+model = LinearModel()
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    loss=myError(),
+    metrics=[myMetric()]
+)
+model.fit(X, y, batch_size=6, epochs=10000)
+print(model.variables)
+
+X_test = tf.constant([[5.1], [6.1]])
+res = model.predict(X_test)
+print(res)
