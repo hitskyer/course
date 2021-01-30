ef49eae8ca43ee29519ee617ec99af7b3374b3ab
kobe_24o <mingmingchen1989@163.com>
2021-01-31 00:15:24 +0800

    Update tf_8_tfdata_catVSdog.py

--- dnn/chenmingming/tf2/tf_8_tfdata_catVSdog.py
+++ dnn/chenmingming/tf2/tf_8_tfdata_catVSdog.py
@@ -1,18 +1,21 @@
 # ---------cat vs dog-------------
 import tensorflow as tf
+import pandas as pd
 import os
 
-num_epochs = 10
+num_epochs = 1
 batch_size = 32
 learning_rate = 1e-3
 train_data_dir = "./dogs-vs-cats/train/"
 test_data_dir = "./dogs-vs-cats/test/"
 
 
-def _decode_and_resize(filename, label):
+def _decode_and_resize(filename, label=None):
     img_string = tf.io.read_file(filename)
     img_decoded = tf.image.decode_jpeg(img_string)
     img_resized = tf.image.resize(img_decoded, [256, 256]) / 255.
+    if label==None:
+        return img_resized
     return img_resized, label
 
 
@@ -21,21 +24,17 @@ if __name__ == "__main__":
     train_labels = tf.constant([0 if filename[0] == 'c' else 1
                                 for filename in os.listdir(train_data_dir)])
     train_dataset = tf.data.Dataset.from_tensor_slices((train_filenames, train_labels))
-    train_dataset = train_dataset.map(map_func=_decode_and_resize,
-                                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    train_dataset = train_dataset.shuffle(buffer_size=20000)
+    train_dataset = train_dataset.map(map_func=_decode_and_resize)
+    # train_dataset = train_dataset.shuffle(buffer_size=25000)
     train_dataset = train_dataset.batch(batch_size)
     train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-    model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(256, 256, 3)),
-        tf.keras.layers.MaxPool2D(),
-        tf.keras.layers.Conv2D(32, 5, activation='relu'),
-        tf.keras.layers.MaxPool2D(),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(2, activation='sigmoid')
-    ])
+    model = tf.keras.applications.MobileNetV2(input_shape=(256,256,3), weights=None, include_top=True, classes=2)
+    # model = tf.keras.Sequential([
+    #     basemodel,
+    #     tf.keras.layers.Dense(64, activation='relu'),
+    #     tf.keras.layers.Dense(2, activation='sigmoid')
+    # ])
 
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
@@ -47,9 +46,12 @@ if __name__ == "__main__":
 
     test_filenames = tf.constant([test_data_dir + filename for filename in os.listdir(test_data_dir)])
     test_data = tf.data.Dataset.from_tensor_slices(test_filenames)
-    test_data = test_data.map(_decode_and_resize)
+    test_data = test_data.map(map_func=_decode_and_resize)
     test_data = test_data.batch(batch_size)
 
     print(model.metrics_names)
     ans = model.predict(test_data)
     print(ans)
+    id = list(range(12500))
+    output = pd.DataFrame({'id': id, 'label': tf.argmax(ans, axis=1).numpy()})
+    output.to_csv("submission.csv", index=False)
